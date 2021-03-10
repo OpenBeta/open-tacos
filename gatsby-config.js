572380d93da5b9683f3ebf21f31e2a9fba877442
Viet Nguyen <3805254+vnugent@users.noreply.github.com>
2021-03-10 16:01:51 +0100

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,41 @@
+const resolveConfig = require("tailwindcss/resolveConfig");
+const tailwindConfig = require("./tailwind.config.js");
+
+const fullConfig = resolveConfig(tailwindConfig);
+
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Starter Tailwind`,
+    description: `Gatsby starter styled with Tailwind`,
+    author: `@taylorbryant`,
+  },
+  plugins: [
+    `gatsby-plugin-eslint`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-tailwind`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: fullConfig.theme.colors.white,
+        theme_color: fullConfig.theme.colors.green["500"],
+        display: `minimal-ui`,
+        icon: `src/images/tailwind-icon.png`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-postcss`,
+      options: {
+        postCssPlugins: [
+          require(`tailwindcss`)(tailwindConfig),
+          require(`autoprefixer`),
+          ...(process.env.NODE_ENV === `production`
+            ? [require(`cssnano`)]
+            : []),
+        ],
+      },
+    },
+    `gatsby-plugin-offline`,
+  ],
+};
---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+// See https://tailwindcss.com/docs/configuration for details
+module.exports = {
+  purge: ["./src/**/*.js"],
+  // https://github.com/tailwindlabs/tailwindcss-forms
+  plugins: [require("@tailwindcss/forms")],
+};
