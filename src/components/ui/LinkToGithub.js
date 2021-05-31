cd056a3ecc09009ae89b607fed05449ea2b9773c
Kevin Nadro <kevin.nadro@gmail.com>
2021-05-31 18:58:11 -0500

    darker purpler text hover instead of gray, that was a typo

--- src/components/ui/LinkToGithub.js
+++ src/components/ui/LinkToGithub.js
@@ -6,7 +6,7 @@ function LinkToGithub ({link, docType}) {
       Caught a mistake or want to contribute to the {docType ? docType :'documentation'}? 
       <a target="_blank" rel="noopener noreferrer" href={link}>
         <span
-          className="ml-0.5 hover:underline cursor-pointer hover:text-gray-900 text-purple-600"
+          className="ml-0.5 hover:underline cursor-pointer hover:text-purple-900 text-purple-600"
         >
           Edit on GitHub!
         </span>
