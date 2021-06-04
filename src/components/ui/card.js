0791e4c8b9cfd6c055d730e04cddf4d36d0e3cd2
Kevin Nadro <kevin.nadro@gmail.com>
2021-06-04 17:52:16 -0500

    removing unused className

--- src/components/ui/card.js
+++ src/components/ui/card.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-function Card({children, onPress, className}) {
+function Card({children, onPress}) {
   return (
     <div
-      className={`card rounded-lg cursor-pointer hover:bg-yellow-50 border ${className ? className : ''}`}
+      className={`card rounded-lg cursor-pointer hover:bg-yellow-50 border`}
       onClick={onPress}
       >
       <div className="m-5">
