926c18f964da79419561b1c93852b82efae248c5
viet nguyen <vietnguyen@noreply>
2021-06-05 20:27:31 +0200

    No need to clone with ssh

--- prebuild.sh
+++ prebuild.sh
@@ -4,15 +4,17 @@
 # Syntax:
 #     ./prebuild [full]
 #
-#     Specify 'full' include the entire dataset 
-
+#     Specify 'full' include the entire dataset
+#
 
 shopt -s extglob
 
 rm -rf opentacos-content
 rm -rf content
 
-git clone --depth 1 --branch nevada git@github.com:OpenBeta/opentacos-content.git opentacos-content
+git clone --depth 1 --branch nevada \
+  https://github.com/OpenBeta/opentacos-content.git \
+  opentacos-content
 
 if [ "$1" != "full" ];
 then
