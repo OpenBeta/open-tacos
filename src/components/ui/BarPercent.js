558f20409fb86d7615fdb46ce9b9dbd4d2ed07ec
Kevin Nadro <nadr0@users.noreply.github.com>
2021-06-05 07:43:25 -0500

    rendering area stats (#21)
    * rendering area stats
    
    * caps on React, when it should be react

---
+++ src/components/ui/BarPercent.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+function BarPercent ({percents=[], colors=[]}) {
+  return (
+    <div>
+      <div className="h-2 w-100 flex">
+        {
+          percents.map((percent, index)=> {
+            const color = colors[index];
+            const moreThanOnePercent = percents.length > 1;
+
+            // Border logic
+            // ( ) single
+            // ( ] [ ) double
+            // ( ] [ ] [ ) triple
+            // ( ] [ ] [ ] [ ) quadruple
+            let borderRounding = 'mr-1';
+            if (moreThanOnePercent && index === 0 ) 
+              borderRounding = 'rounded-l mr-1'
+            if (moreThanOnePercent && index === percents.length - 1) 
+              borderRounding = 'rounded-r'
+            if (!moreThanOnePercent)
+              borderRounding = 'rounded-lg'
+
+            return (
+              <span
+                key={index}
+                className={`${borderRounding} h-2 bg-${color} inline-block`}
+                style={{ width: `${percent}px` }}
+              ></span>
+            );
+          })
+        }
+      </div>
+    </div>
+  )
+}
+
+export default BarPercent;
---
+++ src/js/constants.js
@@ -0,0 +1,6 @@
+export const ClimbTypeToColor = {
+  sport: "indigo-400",
+  trad: "red-700",
+  boulder: "green-700",
+  tr: "yellow-400"
+};
