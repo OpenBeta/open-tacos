429fc99b98a636e21c9e86b4ea97a7dccbbee703
Viet Nguyen <3805254+vnugent@users.noreply.github.com>
2021-06-06 21:40:26 +0200

    Delete old data files. Upload higher res photo (#26)

--- src/components/ui/LandingHero.js
+++ src/components/ui/LandingHero.js
@@ -11,10 +11,9 @@ export default function LandingHero() {
         alt="Indian Creek at sunset"
         layout="fullWidth"
         placeholder="blurred"
-        aspectRatio={16/9}
+        quality="90"
         style={{
           gridArea: "1/1",
-          maxHeight: 700,
         }}
       />
       <div
@@ -29,8 +28,8 @@ export default function LandingHero() {
       >
         {/* In order to create full-screen effect this hero component is placed outside
         of global Layout. Make sure the container's width below is the same Layout's */}
-        <div className="w-full max-w-4xl mx-auto md:px-8 md:py-20">
-          <h1 className="text-5xl text-white antialiased tracking-tight leading-tight mb-2">
+        <div className="w-full max-w-4xl mx-auto md:px-8 md:py-20 sm:px-4">
+          <h1 className="text-5xl text-white antialiased tracking-tight leading-tight mt-4 mb-2">
             Open collaboration <br /> climbing route catalog
           </h1>
           <TextButton label="Learn more" to="/about"/>
