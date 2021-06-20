c81ad42031b311ce2adb12a50de536e5dde53f83
Viet Nguyen <3805254+vnugent@users.noreply.github.com>
2021-06-20 10:50:08 +0200

    Add WA and enable Gatsby build cache (#34)

---
+++ src/components/USToC.js
@@ -0,0 +1,30 @@
+import React from "react";
+import {Link} from "gatsby"
+
+function USToC() {
+  return (
+    <section>
+      <h4 className="text-xl font-medium my-4">Explore by State</h4>
+      <div class="flex gap-x-4">
+        <div className="text-gray-400">California</div>
+        <div>
+          <Link to="/areas/2c278fe6-c679-4aef-a6e1-085d9d205bab/nevada">
+            Nevada
+          </Link>
+        </div>
+        <div>
+          <Link to="/areas/e69f6a6f-ddb1-4460-89c6-024b110c89a7/oregon">
+            Oregon
+          </Link>
+        </div>
+        <div>
+          <Link to="/areas/1b50d4f9-d6e2-4743-a9aa-97dd102afb9e/washington">
+            Washington
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default USToC;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useStaticQuery, graphql, Link } from "gatsby";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
+import USToc from "../components/USToC";
 import RandomRouteCard from "../components/RandomRouteCard";
 
 function getRandomInt(min, max) {
@@ -51,20 +52,8 @@ function IndexPage() {
         keywords={[`gatsby`, `tailwind`, `react`, `tailwindcss`]}
         title="Home"
       />
-      <h4 className="text-xl font-medium my-4">Explore by States</h4>
-      <div class="flex gap-x-4">
-        <div className="text-gray-400">California</div>
-        <div>
-          <Link to="/areas/2c278fe6-c679-4aef-a6e1-085d9d205bab/nevada">
-            Nevada
-          </Link>
-        </div>
-        <div>
-          <Link to="/areas/e69f6a6f-ddb1-4460-89c6-024b110c89a7/oregon">
-            Oregon
-          </Link>
-        </div>
-      </div>
+
+      <USToc />
 
       <h2 className="text-xl font-medium mt-12 mb-4">
         Randomly Featured Route
