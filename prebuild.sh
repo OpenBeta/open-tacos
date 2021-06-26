66bf801cb901e7eae6b7af06c08aef6c6545f69d
viet nguyen <vietnguyen@noreply>
2021-06-26 16:28:12 +0200

    Create symlink for full build

--- prebuild.sh
+++ prebuild.sh
@@ -7,7 +7,9 @@
 #     Specify 'full' include the entire dataset
 #
 
-rm -rf content/*
+rm -rf content
+mkdir content
+
 rm -rf opentacos-content
 
 git clone --depth 1 --branch develop \
@@ -23,8 +25,10 @@ then
   find . -mindepth 2 -maxdepth 4  -name "index*"  -exec rsync -aR "{}" $target \;
 
   # Copy only a few specific areas
-  mkdir -p $target/$target
   rsync -aR ./USA/Oregon/Central\ Oregon/Smith\ Rock $target
   rsync -aR ./USA/Nevada/Southern\ Nevada/Red\ Rock $target
   popd
+else
+  # create a symlink to the entire content repo
+  ln -s opentacos-content/content content
 fi
