06207f33ef1d6a88476862ee9ce192d194878b93
Viet Nguyen <3805254+vnugent@users.noreply.github.com>
2021-07-05 21:27:09 +0200

    Correct flag and update readme (#46)

--- src/components/ui/shortcodes.js
+++ src/components/ui/shortcodes.js
@@ -9,11 +9,11 @@ export const Header = (props) => (
  * <h1> short code to be used in templates
  */
 export const h1 = (props) => (
-  <h1 {...props} className="text-3xl font-medium mt-8 mb-1" />
+  <h1 {...props} className="text-3xl font-medium mt-8 mb-2" />
 );
 
 export const h2 = (props) => (
-  <h2 {...props} className="text-xl font-bold mt-8 mb-1" />
+  <h2 {...props} className="text-xl font-bold mt-10 mb-1" />
 );
 
 export const p = (props) => <p {...props} className="my-4" />;
@@ -28,7 +28,7 @@ export const ol = (props) => (
   <ol {...props} className="list-inside list-decimal" />
 );
 
-export const blockquote =(props) => (<blockquote {...props} className="border-l-4 border-gray-200 pl-6"/>)
+export const blockquote =(props) => (<blockquote {...props} className="border-l-4 border-gray-200 pl-6 my-6"/>)
 
 
 export const pre = (props) => (<pre {...props} className="font-mono text-sm rounded-xl bg-yellow-50 p-4"/>)
