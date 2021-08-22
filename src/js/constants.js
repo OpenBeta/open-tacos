af56bbaa04c21d8ee2edbec6e2abb6ff5903bbda
Kevin Nadro <nadr0@users.noreply.github.com>
2021-08-22 03:33:15 -0500

    Nadro/gatsby envs (#57)
    * refactoring out the variables into a constant variable
    * reverting newline commits

--- src/js/constants.js
+++ src/js/constants.js
@@ -4,3 +4,12 @@ export const ClimbTypeToColor = {
   boulder: "green-700",
   tr: "yellow-400"
 };
+
+// Complains about SSR render, need to check for window object
+export const Auth0Config = {
+  domain: "dev-fmjy7n5n.us.auth0.com",
+  clientId: "0Eth3vOcH6IY75szP00kVQmQt8KhzZmL",
+  redirectUri: typeof window !== "undefined" ? window.location.origin + '/edit' : 'ran in node.js',
+  audience: "https://dev-fmjy7n5n.us.auth0.com/api/v2/",
+  scope: "read:current_user update:current_user_metadata"
+};
