2fd8d4744138a6a5560c358e7399b65df18945ce
Kendra Gibbons <kendra.gibbons@plangrid.com>
2022-01-03 05:16:28 +0100

    Area page additions (#147)
    * Area page additions
    * adding in basic info to area page
    * Moving icons away from direct import next didn't like it, I didn't want to deal with another package
    * SEO > SeoTags
    * turning off lint checks
    * Adding Climbs
    * Adding back in Breadcrumbs
    * Index Page
    * Leaf / Edge Areas
    Navigation from US > oregon to crag
    * Climb pages

---
+++ src/components/ui/EditButton.tsx
@@ -0,0 +1,27 @@
+
+import React from 'react'
+
+interface EditButtonProps {
+  icon?: React.ReactElement
+  label?: string
+  classes?: string
+  rawPath: string
+}
+
+const EditButton: React.FC<EditButtonProps> = ({ icon, label, classes, rawPath }) => {
+  const iconClass = (icon != null) ? 'px-4' : ''
+  return (
+    <button
+      className={`btn whitespace-nowrap ${classes ?? 'btn-secondary'} ${iconClass
+        }`}
+      onClick={async () => {
+        // @gibboj was gatsby.navigate(/edit?file=${rawPath}/index.md`)
+        console.log(`/edit?file=${rawPath}/index.md`)
+      }}
+    >
+      <span className='mr-2'>{icon}</span>
+      {label}
+    </button>
+  )
+}
+export default EditButton
--- src/js/types.ts
+++ src/js/types.ts
@@ -1,3 +1,57 @@
+
+export interface AreaMetadataType {
+  leaf: boolean
+  lat: number
+  lng: number
+  left_right_index: number
+  mp_id: string
+  area_id: string
+}
+
+export interface ClimbMetadataType {
+  lat: number
+  lng: number
+  left_right_index: string
+  mp_id: string
+  climb_id: string
+}
+export interface ClimbDiscipline {
+  boulder: boolean
+  alpine: boolean
+  tr: boolean
+  ice: boolean
+  trad: boolean
+  mixed: boolean
+}
+
 export interface Climb {
-  frontmatter
+  name: string
+  fa: string
+  yds: string
+  metadata: ClimbMetadataType
+  type: ClimbDiscipline
+  content: {
+    description: string
+    location: string
+    protection: string
+  }
+}
+
+export interface AreaType {
+  area_name: string
+  pathTokens: string[]
+  metadata: AreaMetadataType
+  content: {
+    description: string
+  }
+  children: AreaType[]
+  climbs: Climb[]
+}
+
+export interface AreaResponseType {
+  areas: AreaType[]
+}
+
+export interface ClimbResponseType {
+  climbs: Climb[]
 }
