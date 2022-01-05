58fa376fa2e24ef7fba3fb1ce42c7b5c7dd0010d
Viet Nguyen <3805254+vnugent@users.noreply.github.com>
2022-01-05 23:40:34 +0100

    [WIP] new search widget (#164)
    * [WIP] new search widget
    * Connect to Algolia. Display results.

---
+++ src/components/search/ClimbSearch.tsx
@@ -0,0 +1,52 @@
+import algoliasearch from 'algoliasearch/lite'
+import { getAlgoliaResults } from '@algolia/autocomplete-preset-algolia'
+import { useRouter } from 'next/router'
+import Autocomplete from './Autocomplete'
+import { Climb } from '../../js/types'
+import ResultItem from './ResultItem'
+
+const searchClient = algoliasearch(
+  'G7NJXRGX3U',
+  '2ab92f2712962f723f109fa082832c6a'
+)
+
+const ClimbSearch = (): JSX.Element => {
+  const router = useRouter()
+  return (
+    <Autocomplete
+      placeholder='Try &ldquo;Nine Gallon Buckets&rdquo;, &ldquo;technical crimpy&rdquo;, or &ldquo;Alan Watts&rdquo;'
+      getSources={({ query }) => [
+        {
+          sourceId: 'climbs',
+          getItems: () => {
+            return getAlgoliaResults({
+              searchClient,
+              queries: [
+                {
+                  indexName: 'climbs',
+                  query
+                }
+              ]
+            })
+          },
+          navigator: {
+            async navigate ({ itemUrl }) {
+              await router.push(itemUrl)
+            }
+          },
+          getItemUrl ({ item }: {item: Climb}) {
+            return `/climbs/${item.metadata.climb_id}`
+          },
+
+          templates: {
+            item ({ item, components }) {
+              return <ResultItem router={router} {...item} />
+            }
+          }
+
+        }]}
+    />
+  )
+}
+
+export default ClimbSearch
