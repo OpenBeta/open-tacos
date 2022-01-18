1d86472c0b8cf0841c5b125d546e2a7b68f28359
Kendra Gibbons <kendra.gibbons@plangrid.com>
2022-01-18 07:52:05 +0100

    Explore Page + map bug fixes (#184)
    * Bug fixes
    * Update aggregate data to match graphql changes
    * explore cards on index page, images sorced from CC
    * tslint fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,12 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Layout from '../components/layout'
 import SeoTags from '../components/SeoTags'
-import USToC from '../components/USToC'
 
 import { gql } from '@apollo/client'
 import { graphqlClient } from '../js/graphql/Client'
 import { GetStaticProps } from 'next'
 import { IndexResponseType } from '../js/types'
+import FeatureCard from '../components/ui/FeatureCard'
 
 const Home: NextPage<IndexResponseType> = ({ areas, area }) => {
   return (
@@ -24,30 +24,36 @@ const Home: NextPage<IndexResponseType> = ({ areas, area }) => {
       </Head>
 
       <Layout layoutClz='layout-wide'>
-        <USToC areas={area.children} />
+        <h1 className='mt-12'>Explore</h1>
+        <div className='grid grid-cols-1 md:grid-cols-3 md:gap-x-3 gap-y-3'>
+          {areas.map(area => <FeatureCard key={area.metadata.area_id} area={area} />)}
+        </div>
       </Layout>
     </>
   )
 }
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const query = gql`query UsaAreas( $uuid: String) {
-    area(uuid: $uuid) {
+  const query = gql`query UsaAreas( $filter: Filter) {
+    areas(filter: $filter) {
       area_name
+      pathTokens
+      totalClimbs
+      aggregate {
+        byType {
+          label
+          count
+        }
+        byGrade {
+          label
+          count
+        }
+      }
       metadata {
         lat
         lng
         area_id
-        leaf
-      }
-      children {
-        area_name
-        metadata {
-          lat
-          lng
-          area_id
-          leaf
-        }
+        
       }
     }
   }`
@@ -55,7 +61,17 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   const rs = await graphqlClient.query<IndexResponseType>({
     query,
     variables: {
-      uuid: 'd5599113-a4cb-4a68-b588-bd2c1185d131'
+      filter: {
+        field_compare: [{
+          field: 'totalClimbs',
+          num: 200,
+          comparison: 'gt'
+        }, {
+          field: 'density',
+          num: 0.03,
+          comparison: 'gt'
+        }]
+      }
     }
   })
 
