24c7de02700b0cd711618a6ff278c55e78c55b12
viet nguyen <vietnguyen@noreply>
2022-02-01 20:02:50 -0600

    [WIP] make search box more prominent

--- src/components/header.js
+++ src/components/header.js
@@ -1,21 +1,53 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Image from 'next/image'
 import ClimbSearch from './search/ClimbSearch'
 
 function Header () {
   const [isExpanded, toggleExpansion] = useState(false)
+
+  const [direction, setDirection] = useState('top')
+
+  const controlDirection = () => {
+    if (window.scrollY > 200) {
+      setDirection('down')
+      setExpanded(false)
+    } else {
+      setDirection('top')
+    }
+    // oldScrollY = window.scrollY
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', controlDirection)
+    return () => {
+      window.removeEventListener('scroll', controlDirection)
+    }
+  }, [])
+
+  const [expanded, setExpanded] = useState(direction)
+  useEffect(() => {
+    setExpanded(direction === 'top')
+  }, [direction])
+
+  const onClick = () => setExpanded(!expanded)
+  const onBlur = () => {
+    console.log('blur')
+    expanded && setExpanded(false)
+  }
+
+  console.log(direction)
   return (
     <header
-      className={`fixed top-0 z-50 border-b w-full ${isExpanded ? 'bg-gray-50 border-b-2 border-black filter drop-shadow-md' : 'bg-white'
+      className={`fixed top-0 z-20 border-b w-full transition-all duration-300 ease-in-out ${expanded ? ' lg:h-36 ' : 'lg:h-16'} ${isExpanded ? 'bg-gray-800 border-b-2 border-black filter drop-shadow-md' : 'bg-gray-800'
         }`}
     >
-      <div className='flex flex-wrap items-center justify-between max-w-screen-2xl p-4 lg:py-2 mx-auto'>
-        <div className='grow flex flex-nowrap items-center gap-x-4'>
-          <a href='/' className='hidden lg:inline-block'>
-            <Image className='cursor-pointer' src='/tortilla.png' height={32} width={32} />
-          </a>
-          <ClimbSearch />
-        </div>
+      <div className='flex flex-wrap items-center justify-between max-w-screen-2xl px-4 py-4 mx-auto'>
+        <a href='/' className='hidden md:flex flex-nowrap items-center gap-x-2'>
+          <div><Image className='cursor-pointer' src='/tortilla.png' height={32} width={32} /></div>
+          <span className='font-bold text-custom-primary'>OpenTacos</span>
+        </a>
+        <ClimbSearch expanded={expanded} onClick={onClick} onBlur={onBlur} />
+
         <button
           className='items-center block px-3 py-2 text-black border border-white rounded lg:hidden'
           onClick={() => toggleExpansion(!isExpanded)}
@@ -30,7 +62,7 @@ function Header () {
           </svg>
         </button>
         <nav
-          className={`text-2xl lg:text-sm ${isExpanded ? 'block mt-4 divide-y' : 'hidden'
+          className={`text-primary-contrast text-2xl lg:text-sm ${isExpanded ? 'block mt-4 divide-y' : 'hidden'
             } lg:flex lg:justify-end w-full lg:w-auto`}
         >
           {[
--- src/components/search/ClimbSearch.tsx
+++ src/components/search/ClimbSearch.tsx
@@ -1,51 +1,81 @@
+import React from 'react'
 import algoliasearch from 'algoliasearch/lite'
 import { getAlgoliaResults } from '@algolia/autocomplete-preset-algolia'
 import { useRouter } from 'next/router'
 import Autocomplete from './Autocomplete'
 import { ClimbAlgoliaType } from '../../js/types'
 import ResultItem from './ResultItem'
+import SearchIcon from '../../assets/icons/search.svg'
 
 const searchClient = algoliasearch(
   'G7NJXRGX3U',
   process.env.NEXT_PUBLIC_ALGOLIA_API_KEY
 )
 
-const ClimbSearch = (): JSX.Element => {
+interface ClimbSearchProps {
+  expanded: boolean
+  onClick: any
+  onBlur: any
+}
+
+const ClimbSearch = ({ expanded, onClick, onBlur }: ClimbSearchProps): JSX.Element => {
   const router = useRouter()
+
   return (
-    <Autocomplete
-      placeholder='Try &ldquo;Levitation 29&rdquo;, &ldquo;technical crimpy&rdquo;, or &ldquo;Lynn Hill&rdquo;'
-      getSources={({ query }) => [
-        {
-          sourceId: 'climbs',
-          getItems: () => {
-            return getAlgoliaResults({
-              searchClient,
-              queries: [
-                {
-                  indexName: 'climbs',
-                  query
+    <div className='fixed top-0 left-0 w-screen horizontal-center z-10'>
+      <div className='max-w-screen-lg horizontal-center w-full flex flex-col items-center '>
+        <FakeSearchBox onClick={onClick} expanded={expanded} />
+        {expanded && <div className='py-4 text-secondary-contrast'>Find climbs by name, characteristics or FA</div>}
+        <div className={`${expanded ? 'w-full horizontal-center ' : 'hidden'}`}>
+          <Autocomplete
+            placeholder='Try &ldquo;Levitation 29&rdquo;, &ldquo;technical crimpy&rdquo;, or &ldquo;Lynn Hill&rdquo;'
+            getSources={({ query }) => [
+              {
+                sourceId: 'climbs',
+                getItems: () => {
+                  return getAlgoliaResults({
+                    searchClient,
+                    queries: [
+                      {
+                        indexName: 'climbs',
+                        query
+                      }
+                    ]
+                  })
+                },
+                navigator: {
+                  async navigate ({ itemUrl }) {
+                    await router.push(itemUrl)
+                  }
+                },
+                getItemUrl ({ item }: {item: ClimbAlgoliaType}) {
+                  return `/climbs/${item.objectID}`
+                },
+
+                templates: {
+                  item ({ item, components }) {
+                    return <ResultItem router={router} {...item} />
+                  }
                 }
-              ]
-            })
-          },
-          navigator: {
-            async navigate ({ itemUrl }) {
-              await router.push(itemUrl)
-            }
-          },
-          getItemUrl ({ item }: {item: ClimbAlgoliaType}) {
-            return `/climbs/${item.objectID}`
-          },
 
-          templates: {
-            item ({ item, components }) {
-              return <ResultItem router={router} {...item} />
-            }
-          }
+              }]}
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
 
-        }]}
-    />
+export const FakeSearchBox = ({ onClick, expanded }: {onClick: any, expanded: boolean}): JSX.Element => {
+  return (
+    <div className='py-2'>
+      <div onClick={onClick} className={`${expanded ? 'hidden h-0' : 'block'} cursor-pointer mt-0.5 border border-gray-200 shadow-sm rounded-full flex flex-row items-center gap-x-4 py-0.5 bg-white`}>
+        <div className='pl-4 pr-8 text-sm'>Start your search</div>
+        <div className='rounded-full bg-custom-primary p-2 mr-1'>
+          <SearchIcon className='stroke-white' />
+        </div>
+      </div>
+    </div>
   )
 }
 
