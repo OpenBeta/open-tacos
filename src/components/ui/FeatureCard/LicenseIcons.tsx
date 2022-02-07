162ec0a136768d81651209462886e2e93902dd0f
Kendra Gibbons <kendra.gibbons@gmail.com>
2022-02-07 22:50:55 +0100

    Feature Card Improvements
    * Better CC compliance
    * Fallback search for images

---
+++ src/components/ui/FeatureCard/LicenseIcons.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Cc, By, Nc, Sa, Nd, Zero } from '../../../assets/icons/cc-icons'
+
+import { OpenverseImage } from '.'
+
+function LicenseIcons ({ image }: { image: OpenverseImage }): JSX.Element {
+  const size = { width: 25, height: 25 }
+  const getIcon = (license: string): JSX.Element => {
+    switch (license.toUpperCase()) {
+      case 'BY':
+        return (<By {...size} />)
+      case 'CC0':
+        return (<><Cc {...size} /><Zero {...size} /></>)
+      case 'BY-NC-ND':
+        return (<><By {...size} /><Nc {...size} /><Nd {...size} /></>)
+      case 'BY-SA':
+        return (<><By {...size} /><Sa {...size} /></>)
+      case 'BY-NC-SA':
+        return (<><By {...size} /><Nc {...size} /><Sa {...size} /></>)
+      default:
+        return null
+    }
+  }
+  return (
+    <div
+      onClick={() => {
+        window.open(image.license_url, '_blank')
+        return false
+      }} className='flex flex-row-reverse m-2'
+    >
+      {getIcon(image.license)}
+    </div>
+  )
+}
+
+export default LicenseIcons
