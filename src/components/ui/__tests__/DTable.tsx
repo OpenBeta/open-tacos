e06670df7bccc3d6cbed152a3dd8e45414ea2b19
viet nguyen <vietnguyen@noreply>
2022-04-22 11:34:21 +0200

    Mock cragFiltersStore locally

--- src/components/ui/__tests__/DTable.tsx
+++ src/components/ui/__tests__/DTable.tsx
@@ -1,28 +1,79 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
-import DTable from '../DTable'
+import { render, screen } from '@testing-library/react'
 
 const byDisciplineAggBoulder = {
-  __typename: 'CountByDisciplineType',
-  sport: null,
+  sport:
+    {
+      total: 10,
+      bands: {
+        beginner: 6,
+        intermediate: 6,
+        expert: 0,
+        advance: 0
+      }
+    },
   trad: null,
   boulder: {
-    __typename: 'DisciplineStatsType',
     total: 3,
     bands: {
-      __typename: 'CountByGradeBand',
-      advance: 1,
       beginner: 2,
+      intermediate: 0,
       expert: 0,
-      intermediate: 0
+      advance: 1
     }
   },
   tr: null
 }
 
-test('DTable renders without crashing', () => {
-  const { container } = render(<DTable byDisciplineAgg={byDisciplineAggBoulder} />)
-  expect(true).toBe(true)
-  console.log(container)
+jest.mock('../../../js/stores/index', () => ({
+  cragFiltersStore: {
+    get: {
+      trad: () => false,
+      sport: jest.fn().mockReturnValue(true),
+      boulder: jest.fn().mockReturnValue(true),
+      tr: () => true,
+      freeBandRange: () => [0, 1],
+      boulderBandRange: () => [0, 1]
+    }
+  }
+}))
+
+const mockedStore = jest.requireMock('../../../js/stores/index')
+
+let DTable
+
+beforeAll(async () => {
+  const module = await import('../DTable')
+  DTable = module.default
+})
+
+describe('DTable', () => {
+  test('DTable renders without crashing', () => {
+    render(<DTable byDisciplineAgg={byDisciplineAggBoulder} />)
+
+    const boulderingRow = screen.getByRole('row', { name: /boulder/i })
+
+    expect(boulderingRow.childNodes.length).toEqual(5)
+
+    expect(boulderingRow.childNodes[0]).toHaveTextContent('boulder')
+    expect(boulderingRow.childNodes[1]).toHaveTextContent('2') // Beginner
+    expect(boulderingRow.childNodes[2]).toHaveTextContent('0')
+    expect(boulderingRow.childNodes[3]).toHaveTextContent('1') // Advanced
+    expect(boulderingRow.childNodes[4]).toHaveTextContent('0')
+
+    const sportRow = screen.queryByRole('row', { name: /trad/i })
+    expect(sportRow).toBeNull()
+  })
+
+  test('DTable highlights my disciplines', () => {
+    (mockedStore.cragFiltersStore.get.boulder as jest.Mock).mockReturnValue(false) // user unchecks 'bouldering'
+    render(<DTable byDisciplineAgg={byDisciplineAggBoulder} />)
+
+    const boulderingRow = screen.getByRole('row', { name: /boulder/i })
+    expect(boulderingRow.childNodes[1]).not.toHaveClass('dtable-highlight')
+
+    const sportRow = screen.getByRole('row', { name: /sport/i })
+    expect(sportRow.childNodes[1]).toHaveClass('dtable-highlight')
+  })
 })
