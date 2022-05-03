caf78dbf44f45231ec7d1f8912549fccf3e7edb3
Nathan <nhearnsberger@gmail.com>
2022-05-03 14:08:38 -0500

    add menu popover on mobile

--- src/components/MobileAppBar.tsx
+++ src/components/MobileAppBar.tsx
@@ -1,8 +1,10 @@
 import Link from 'next/link'
 import CragFinder from './search/CragFinder'
 import MobileNavBar from './ui/MobileNavBar'
-import { HomeIcon } from '@heroicons/react/outline'
+import { HomeIcon, MenuIcon } from '@heroicons/react/outline'
 import MobileFilterBar from './finder/filters/MobileFilterBar'
+import { Popover } from '@headlessui/react'
+import { Button } from './ui/BaseButton'
 
 export default function MobileAppBar (): JSX.Element {
   return (
@@ -11,16 +13,21 @@ export default function MobileAppBar (): JSX.Element {
         branding={<Branding />}
         home={<Home />}
         search={<CragFinder />}
-        more={<div>&nbsp;</div>}
+        more={<More />}
       />
       <MobileFilterBar />
     </>
-
   )
 }
 
 const Home = (): JSX.Element => {
-  return (<Link href='/'><a><HomeIcon className='text-primary' /></a></Link>)
+  return (
+    <Link href='/'>
+      <a>
+        <HomeIcon className='text-primary' />
+      </a>
+    </Link>
+  )
 }
 
 const Branding = (): JSX.Element => {
@@ -28,5 +35,26 @@ const Branding = (): JSX.Element => {
     <Link href='/'>
       <a className='font-semibold text-lg text-primary pt-1'>OpenTacos</a>
     </Link>
+  )
+}
+
+const More = (): JSX.Element => {
+  return (
+    <Popover>
+      <Popover.Button className='flex center-items'>
+        <MenuIcon className='w-8 h-8' />
+      </Popover.Button>
+
+      <Popover.Panel className='absolute z-20 right-0 mt-2 p-6 bg-white rounded-md'>
+        <div className='grid'>
+          <Button href='/about' label='About' />
+          <Button
+            href='https://discord.gg/2A2F6kUtyh'
+            label='Discord'
+            variant={Button.VARIANT_SOLID_SECONDARY}
+          />
+        </div>
+      </Popover.Panel>
+    </Popover>
   )
 }
