fb3a2d6a33af75f5ca162475e2d12e8ef1608136
CocoisBuggy <g.colinharley@gmail.com>
2022-05-15 10:09:47 +0200

    Simple but stylistically consistent 404 Page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,34 +1,33 @@
 import React from 'react'
 import Header from '../components/Header'
-// import guyOnBoulder from '../images/guy_on_boulder.png'
-// import guyOnBoulderBG from '../images/guy_on_boulder_bg.png'
-// import ImagePlane from '../components/ui/ImagePlane'
-
-// <ImagePlane foregroundImage={guyOnBoulder} backgroundImage={guyOnBoulderBG}>
-//         <div className='text-center mt-8'>
-//           <div className='flex justify-center'>
-//             <div className='bg-yellow-100 italic mb-2'>
-//               404
-//             </div>
-//           </div>
-//
-//           <div className='bg-white text-6xl tracking-wide font-bold px-2 py-1'>
-//             Page not found
-//           </div>
-//
-//           <div className='mt-2 bg-white'>
-//             You'll have to look somewhere else, it's not here
-//           </div>
-//         </div>
-//       </ImagePlane>
 
 function NotFoundPage (): JSX.Element {
   return (
-    <div className='h-screen bg-gray-50 overflow-hidden'>
-      <div className='bg-white'>
+    <>
+      <div className='absolute w-full h-screen bg-gray-50 overflow-hidden p-2'>
+        <div className='flex items-center align-middle justify-center h-screen'>
+          <div className='text-center mt-8'>
+            <div className='flex justify-center'>
+              <div className='bg-yellow-100 italic mb-2'>
+                404
+              </div>
+            </div>
+
+            <div className='bg-white text-4xl md:text-7xl tracking-wide font-bold px-2 py-1'>
+              Page not found
+            </div>
+
+            <div className='mt-2 bg-white'>
+              You'll have to look somewhere else, it's not here
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className='absolute w-full'>
         <Header />
       </div>
-    </div>
+    </>
   )
 }
 
