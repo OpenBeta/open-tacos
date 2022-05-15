7f31803b64f2b61ad8a8bb8299c96eb27f464d49
CocoisBuggy <g.colinharley@gmail.com>
2022-05-15 12:52:56 +0200

    - Texture was distracting - removed - Footer included - better div layout

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
-import Header from '../components/Header'
+import { getNavBarOffset } from '../components/Header'
+import Layout from '../components/layout'
+import useResponsive from '../js/hooks/useResponsive'
 
 function NotFoundPage (): JSX.Element {
+  const { isMobile, isTablet, isDesktop } = useResponsive()
+  const navbarOffset = getNavBarOffset({ isMobile, isTablet, isDesktop })
   return (
-    <>
-      <div className='absolute w-full h-screen bg-gray-50 overflow-hidden p-2'>
-        <div className='flex items-center align-middle justify-center h-screen'>
+    <Layout contentContainerClass=''>
+      <div className='w-full  bg-gray-50 p-2' style={{ height: `calc(100vh - ${navbarOffset}px)` }}>
+        <div className='flex items-center align-middle justify-center h-full'>
           <div className='text-center mt-8'>
             <div className='flex justify-center'>
               <div className='bg-yellow-100 italic mb-2'>
@@ -13,21 +17,17 @@ function NotFoundPage (): JSX.Element {
               </div>
             </div>
 
-            <div className='bg-white text-4xl md:text-7xl tracking-wide font-bold px-2 py-1'>
+            <div className='text-4xl md:text-7xl tracking-wide font-bold px-2 py-1'>
               Page not found
             </div>
 
-            <div className='mt-2 bg-white'>
+            <div className='mt-2'>
               You'll have to look somewhere else, it's not here
             </div>
           </div>
         </div>
       </div>
-
-      <div className='absolute w-full'>
-        <Header />
-      </div>
-    </>
+    </Layout>
   )
 }
 
