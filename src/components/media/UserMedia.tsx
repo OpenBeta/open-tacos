fbccf24dfe91ed3759b7092348ae302e8a7ccead
viet nguyen <vietnguyen@noreply>
2022-05-18 18:10:48 +0200

    Fix build break

---
+++ src/components/media/UserMedia.tsx
@@ -0,0 +1,41 @@
+import { useState, useCallback } from 'react'
+import Image from 'next/image'
+
+import { SIRV_CONFIG } from '../../js/sirv/SirvClient'
+import TagList from './TagList'
+import { MediaTag } from '../../js/types'
+
+interface UserMediaProps {
+  imageInfo: any
+  onClick: (props: any) => void
+  onTagDeleted?: (props?: any) => void
+  tagList: MediaTag[]
+}
+
+export default function UserMedia ({ imageInfo, onClick, tagList, onTagDeleted }: UserMediaProps): JSX.Element {
+  const [hovered, setHover] = useState(false)
+  const imgUrl = `${SIRV_CONFIG.baseUrl}${imageInfo.filename as string}?format=webp&thumbnail=300&q=90`
+
+  const onClickHandler = useCallback((event) => {
+    onClick({ mouseXY: [event.clientX, event.clientY], imageInfo })
+  }, [])
+
+  return (
+    <div
+      className='cursor-pointer block mx-0 my-4 md:m-4 w-[300px] h-[300px] relative'
+      onClick={onClickHandler}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
+      <Image
+        src={imgUrl}
+        width={300}
+        height={300}
+      />
+      {tagList?.length > 0 &&
+        <div className='absolute inset-0 flex flex-col justify-end'>
+          <TagList hovered={hovered} list={tagList} onDeleted={onTagDeleted} />
+        </div>}
+    </div>
+  )
+}
