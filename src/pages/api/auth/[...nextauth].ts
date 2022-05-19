a675c5a42e9edf2a970e2ce0f9cb5bfcf3a96e69
Nathan <nhearnsberger@gmail.com>
2022-05-19 05:28:24 -0500

    require client secret in prod

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,14 +1,20 @@
 import NextAuth from 'next-auth'
 import Auth0Provider from 'next-auth/providers/auth0'
 
+const clientSecret = process.env.AUTH0_CLIENT_SECRET ?? ''
+
+if (process.env.NODE_ENV === 'production' && clientSecret.length === 0) {
+  throw new Error('AUTH0_CLIENT_SECRET is required in production')
+}
+
 export default NextAuth({
   providers: [
-    // @ts-expect-error next-auth expects clientSecret but it is not used as token_endpoint_auth_method is set to none
     Auth0Provider({
       clientId: process.env.AUTH0_CLIENT_ID,
+      clientSecret,
       issuer: process.env.AUTH0_DOMAIN,
       client: {
-        token_endpoint_auth_method: 'none'
+        token_endpoint_auth_method: clientSecret.length === 0 ? 'none' : 'client_secret_basic'
       }
     })
   ]
