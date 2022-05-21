442f6ec1f8b9a038d239291f5e6626ebbc8694b5
viet nguyen <vietnguyen@noreply>
2022-05-21 15:29:02 +0200

    Debug test

--- src/js/sirv/__tests__/SirvClient.ts
+++ src/js/sirv/__tests__/SirvClient.ts
@@ -3,6 +3,8 @@ import { getToken, getUserImages } from '../SirvClient'
 
 test('Sirv client', async () => {
   const token = await getToken()
+  console.log('#token', token != null)
   const list = await getUserImages('abe96612-2742-43b0-a128-6b19d4e4615f', token)
+  console.log('#getImages', list)
   expect(list.mediaList.length).toBeGreaterThan(0)
 })
