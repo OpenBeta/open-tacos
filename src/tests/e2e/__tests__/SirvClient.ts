da25ab207c2ab4e621e6dd457713bbefe4b066ca
viet nguyen <vietnguyen@noreply>
2022-05-23 16:16:50 +0200

    Make e2e tests optional

--- src/tests/e2e/__tests__/SirvClient.ts
+++ src/tests/e2e/__tests__/SirvClient.ts
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom/extend-expect'
-import { getToken, getUserImages, SIRV_CONFIG } from '../SirvClient'
+import { getToken, getUserImages, SIRV_CONFIG } from '../../../js/sirv/SirvClient'
 
 beforeAll(() => {
   expect(SIRV_CONFIG.clientSecret).not.toBeNull()
@@ -7,7 +7,7 @@ beforeAll(() => {
   expect(SIRV_CONFIG.clientId).not.toBeNull()
 })
 
-test('Sirv client', async () => {
+test('Sirv API can read photos', async () => {
   const token = await getToken()
   const list = await getUserImages('abe96612-2742-43b0-a128-6b19d4e4615f', token)
   expect(list.mediaList.length).toBeGreaterThan(0)
