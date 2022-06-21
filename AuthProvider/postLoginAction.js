fc37d954234433cd593300f064164f38e1519ef0
Viet Nguyen <3805254+vnugent@users.noreply.github.com>
2022-06-21 11:11:35 +0200

    [hotfix] fix post login action (#379)

--- AuthProvider/postLoginAction.js
+++ AuthProvider/postLoginAction.js
@@ -1,7 +1,5 @@
 /**
 * Handler that will be called during the execution of a PostLogin flow.
-* Need to add 'unique-names-generator' dependency.
-* See https://auth0.com/docs/customize/actions/manage-dependencies
 *
 * @param {Event} event - Details about the user and the context in which they are logging in.
 * @param {PostLoginAPI} api - Interface whose methods can be used to change the behavior of the login.
@@ -9,10 +7,27 @@
 exports.onExecutePostLogin = async ({ user }, api) => {
   const { v4: uuidv4 } = require('uuid')
   const { uniqueNamesGenerator, adjectives, colors, animals } = require('unique-names-generator')
-  user.user_metadata.nick = user.user_metadata?.nick ?? uniqueNamesGenerator({ dictionaries: [adjectives, colors, animals] }) // big_red_donkey
 
-  api.user.setUserMetadata('name', user.user_metadata.name || user.name)
-  api.user.setUserMetadata('bio', user.user_metadata.bio || 'Short bio.  Chillwave occaecat slow-carb mustache.')
-  api.user.setUserMetadata('uuid', user.user_metadata.uuid || uuidv4())
+  user.user_metadata.nick = user.user_metadata?.nick ?? uniqueNamesGenerator({ dictionaries: [adjectives, colors, animals] }) // big_red_donkey
+  user.user_metadata.bio = user.user_metadata?.bio ?? ''
+  user.user_metadata.uuid = user.user_metadata?.uuid ?? uuidv4()
+  user.user_metadata.name = user.user_metadata?.name ?? user?.nickname ?? ''
+
+  api.user.setUserMetadata('name', user.user_metadata.name)
+  api.user.setUserMetadata('bio', user.user_metadata.bio)
+  api.user.setUserMetadata('uuid', user.user_metadata.uuid)
   api.user.setUserMetadata('nick', user.user_metadata.nick)
+
+  const ns = 'https://tacos.openbeta.io/'
+  api.idToken.setCustomClaim(ns + 'user_metadata', user.user_metadata)
 }
+
+/**
+* Handler that will be invoked when this action is resuming after an external redirect. If your
+* onExecutePostLogin function does not perform a redirect, this function can be safely ignored.
+*
+* @param {Event} event - Details about the user and the context in which they are logging in.
+* @param {PostLoginAPI} api - Interface whose methods can be used to change the behavior of the login.
+*/
+// exports.onContinuePostLogin = async (event, api) => {
+// };
