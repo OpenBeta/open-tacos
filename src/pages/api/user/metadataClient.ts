48e5a7ca2186101a566ddb836b1f47e238d1552e
Viet Nguyen <3805254+vnugent@users.noreply.github.com>
2022-06-29 12:58:12 +0200

    Generate user id mapping file for new users (#397)
    Co-authored-by: viet nguyen <vietnguyen@noreply>

--- src/pages/api/user/metadataClient.ts
+++ src/pages/api/user/metadataClient.ts
@@ -37,13 +37,6 @@ const createMetadataClient = async (
     return null
   }
 
-  // const currentUserManagementClient = new ManagementClient<any, Auth0UserMetadata>({
-  //   domain: AUTH_CONFIG_SERVER?.issuer.replace('https://', '') ?? '',
-  //   clientId: AUTH_CONFIG_SERVER?.clientId,
-  //   clientSecret: AUTH_CONFIG_SERVER?.clientSecret,
-  //   scope: 'read:users update:users'
-  // })
-
   const getUserMetadata = async (): Promise<Auth0UserMetadata> => {
     const user = await auth0ManagementClient.getUser({ id })
     return reshapeAuth0UserToProfile(user)
