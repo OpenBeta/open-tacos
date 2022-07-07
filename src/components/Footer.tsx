209c0d904ca0dec65ba9369219a6d6c06b3849b4
xavier-kong <kongxianchengxavier@gmail.com>
2022-07-07 21:42:00 +0800

    added icons and slight css fixes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,20 +1,14 @@
+import Image from 'next/image'
+import LinkedIn from "../assets/icons/li.inline.svg";
+import Instagram from "../assets/icons/ig.inline.svg";
+import Github from "../assets/icons/github.inline.svg";
+import Twitter from "../assets/icons/twitter.inline.svg";
+
 function Footer() {
     return (
       <footer className='bg-contrast text-primary'>
-        <nav className='flex justify-between max-w-4xl p-4 mx-auto text-sm md:p-8'>
-          <p className=''>
-            A project by {' '}
-            <a
-            className='font-semibold no-underline'
-            href='https://openbeta.io'
-            target='_blank'
-            rel='noopener noreferrer'
-            >
-              OpenBeta
-            </a>
-          </p>
-
-          <p className=''>
+        <nav className='xl:flex justify-between max-w-7xl p-4 mx-auto text-sm md:p-8'>
+          <p className='md:text-center'>
             Support Email: {' '}
             <a
             className='font-semibold no-underline'
@@ -26,16 +20,26 @@ function Footer() {
             </a>
           </p>
 
-          <p>
+          <p className='md:text-center '>
+            A project by {' '}
             <a
             className='font-semibold no-underline'
-            href='https://github.com/OpenBeta/open-tacos'
+            href='https://openbeta.io'
             target='_blank'
             rel='noopener noreferrer'
             >
-              GitHub
+              OpenBeta
             </a>
           </p>
+
+          <div className="flex justify-center ">
+            <div className="flex flex-row items-center w-48 justify-between">
+              <a href="https://github.com/OpenBeta"><Image src={Github} /></a>
+              <a href="https://www.instagram.com/openbetaproject/"><Image src={Instagram} /></a>
+              <a href="https://twitter.com/OpenBetaHQ"><Image src={Twitter} /></a>
+              <a href="https://www.linkedin.com/company/openbetacollective/"><Image src={LinkedIn} /></a>
+            </div>
+          </div>
         </nav>
       </footer>
     )
