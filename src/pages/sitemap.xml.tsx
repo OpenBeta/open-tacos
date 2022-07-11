8f6cc14fa540e6a0009e26f1d2d813b613f48c1c
Brendan Downing <b.downing.code@gmail.com>
2022-07-11 14:43:27 -0700

    Sitemaps.xml (#424)
    * added sitemap.xml
    * Undiong some changes
    * added sitemap.xml
    * Undiong some changes

---
+++ src/pages/sitemap.xml.tsx
@@ -0,0 +1,100 @@
+import { graphqlClient } from '../js/graphql/Client'
+import { gql } from '@apollo/client'
+import { IndexResponseType } from '../js/types'
+
+const Sitemap = (): void => { }
+
+const query = gql`query UsaAreas( $filter: Filter) {
+    areas(filter: $filter, sort: { totalClimbs: -1 }) {
+      id
+      uuid
+      areaName
+      pathTokens
+      totalClimbs
+      density
+      aggregate {
+        byDiscipline {
+            sport {
+              total
+            }
+            trad {
+              total
+            }
+            boulder {
+              total
+            }
+            tr {
+              total
+            }
+            alpine {
+              total
+            }
+            mixed {
+              total
+            }
+            aid {
+              total
+            }
+          }
+      }
+      metadata {
+        lat
+        lng
+        areaId
+      }
+      media {
+        mediaUrl
+        mediaUuid
+      }
+    }
+  }`
+
+export const getServerSideProps = async ({ res }): Promise<object> => {
+  const baseUrl: string = {
+    development: 'http://localhost:3000',
+    production: 'https://tacos.openbeta.io/'
+  }[process.env.NODE_ENV]
+
+  const { data } = await graphqlClient.query<IndexResponseType>({
+    query,
+    variables: {
+      filter: {
+        field_compare: [{
+          field: 'totalClimbs',
+          num: 400,
+          comparison: 'gt'
+        }, {
+          field: 'density',
+          num: 0.5,
+          comparison: 'gt'
+        }]
+      }
+    }
+  })
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        ${data?.areas
+            .map(({ uuid }) => {
+                return `
+                  <url>
+                    <loc>${baseUrl}/areas/${uuid}</loc>
+                    <changefreq>monthly</changefreq>
+                    <priority>1.0</priority>
+                  </url>
+                `
+            })
+            .join('')}
+      </urlset>
+    `
+
+  res.setHeader('Content-Type', 'text/xml')
+  res.write(sitemap)
+  res.end()
+
+  return {
+    props: {}
+  }
+}
+
+export default Sitemap
