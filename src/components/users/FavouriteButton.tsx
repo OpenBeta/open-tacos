21050511c4d4bf02547581fc4ba855f1ff5addc9
Colin Gale <64557383+CocoisBuggy@users.noreply.github.com>
2022-08-02 10:05:57 +0200

    Functional Favourite system with scoped collections. (#448)
    * Functional Favourite system with scoped collections.
    * Favourite now redirects to login when no session is available. Color is now brand primary
    * bugfix no login button and delete operation producing asymmetric output
    * Some null fixes, and a questionable attempt to make the mechanisms more typesafe in general

---
+++ src/components/users/FavouriteButton.tsx
@@ -0,0 +1,110 @@
+import { signIn, useSession } from 'next-auth/react'
+import React, { useEffect, useState } from 'react'
+import { APIFavouriteCollections } from '../../pages/api/user/fav'
+
+interface Props {
+  climbId?: string
+  areaId?: string
+}
+
+function NoLogin (): JSX.Element {
+  return (
+    <button
+      onClick={async () => await signIn('auth0', { callbackUrl: '/api/user/me' })}
+      className='text-center p-2 px-3 border-2 rounded-xl border-ob-primary transition
+        text-ob-primary hover:bg-slate-700 hover:ring hover:ring-slate-700 ring-offset-2
+        hover:text-white hover:border-slate-700'
+    >
+      ❤️ Login to Favourite
+    </button>
+  )
+}
+
+/** Assuming that there is a generic 'favourites' collection for the time being. */
+const favCollection = 'favourites'
+
+/** Built for rapid single-instancing. This component payloads its own network requests
+ * so don't go making many instances without hoisting the requests into a parent.
+ */
+export default function FavouriteButton ({ climbId, areaId }: Props): JSX.Element | null {
+  const [loading, setLoading] = useState(false)
+  const [isFav, setIsFav] = useState<boolean>(false)
+  const session = useSession()
+
+  useEffect(() => {
+    setLoading(true)
+    fetch('/api/user/fav')
+      .then(async res => await res.json())
+      .then((collections: APIFavouriteCollections) => {
+        if (climbId !== undefined) {
+          const f = collections.climbCollections[favCollection]
+          if (f === undefined) {
+            setIsFav(false)
+            return
+          }
+
+          setIsFav(f.includes(climbId))
+          return // guard block
+        }
+
+        if (areaId !== undefined) {
+          const f = collections.areaCollections[favCollection]
+          if (f === undefined) {
+            setIsFav(false)
+            return
+          }
+
+          setIsFav(f.includes(areaId))
+          // guard block
+        }
+      })
+      .catch(console.error)
+      .finally(() => {
+        setLoading(false)
+      })
+  }, [climbId, areaId])
+
+  const toggle = (): void => {
+    // Choose operation purely on what the current visual
+    // state of the button is.
+    setLoading(true)
+    let method = 'POST'
+    if (isFav) {
+      method = 'DELETE'
+    }
+
+    fetch('/api/user/fav', {
+      method: method,
+      body: JSON.stringify({ climbId, areaId, collection: favCollection })
+    })
+      .then(() => setIsFav(!isFav))
+      .catch(err => console.error({ err }))
+      .finally(() => {
+        setLoading(false)
+      })
+  }
+
+  // If there is some kind of programming error / user is un-authenticated we render the default
+  // interaction-devoid button
+  if ((climbId === undefined && areaId === null) || session.status === 'unauthenticated') {
+    return <NoLogin />
+  }
+
+  return (
+    <button
+      disabled={loading}
+      onClick={toggle}
+      className='text-center p-2 border-2 rounded-xl border-ob-primary transition
+        text-ob-primary hover:bg-ob-primary hover:ring hover:ring-ob-primary ring-offset-2
+        hover:text-white w-64 font-bold'
+    >
+      {loading
+        ? (
+          <span className='animate-pulse'>
+            Working on it...
+          </span>
+          )
+        : (!isFav ? '❤️ Add to Favourites' : '💔 Remove from favourites')}
+    </button>
+  )
+}
---
+++ src/pages/api/user/fav.ts
@@ -0,0 +1,156 @@
+import { NextApiHandler } from 'next'
+import withAuth from '../withAuth'
+import createMetadataClient, { Auth0UserMetadata } from './metadataClient'
+
+type CollectionType = string[]
+type CollectionCollectionType = Map<string, CollectionType>
+/**
+ * Collections are defined as being extremely generic. These are the
+ * specific entity collections that are favourites. Ticks, for example,
+ * may vary in the type of data that needs to be enumerated.
+ */
+interface ReifiedFavouriteCollections {
+  climbCollections: CollectionCollectionType
+  areaCollections: CollectionCollectionType
+}
+
+export interface APIFavouriteCollections {
+  climbCollections: {[key: string]: string[] | undefined }
+  areaCollections: {[key: string]: string[] | undefined}
+}
+
+/** Body params we expect to recieve */
+interface BodyType {
+  climbId?: string | string[]
+  areaId?: string | string[]
+  collection?: string | string[]
+}
+
+function reifyCollections (meta: Auth0UserMetadata): ReifiedFavouriteCollections {
+  return {
+    climbCollections: ((meta?.collections?.climbCollections) != null)
+      ? new Map(Object.entries(meta?.collections?.climbCollections))
+      : new Map(Object.entries({})) as CollectionCollectionType,
+
+    areaCollections: ((meta?.collections?.areaCollections) != null)
+      ? new Map(Object.entries(meta?.collections?.areaCollections))
+      : new Map(Object.entries({})) as CollectionCollectionType
+  }
+}
+
+function backToJSONSafe (collections: ReifiedFavouriteCollections): APIFavouriteCollections {
+  return {
+    climbCollections: Object.fromEntries(collections.climbCollections),
+    areaCollections: Object.fromEntries(collections.areaCollections)
+  }
+}
+
+/** This can be called repeatedly without causing problems.
+ * we interpret the favs as a set, so it's kinda whatever.
+ */
+const handler: NextApiHandler<any> = async (req, res) => {
+  try {
+    const metadataClient = await createMetadataClient(req)
+    if (metadataClient == null) throw new Error('Can\'t create ManagementAPI client')
+
+    /**
+     * within this closure, this meta object will be mutated a fair bit.
+     * At the very end, it will be committed (except in the case of a GET request)
+     */
+    const meta = await metadataClient.getUserMetadata()
+    const collections = reifyCollections(meta)
+    if (req.method === 'GET') {
+      // This is a bit of a hack. We don't want to return the whole metadata object.
+      // We just want to return the favs.
+      res.json(backToJSONSafe(collections))
+      res.end()
+      return
+    }
+
+    const body: BodyType = JSON.parse(req.body)
+
+    if (body.climbId === undefined && body.areaId === undefined) {
+      throw new Error('No climb or area id provided. At least one must be supplied')
+    }
+
+    if (body.collection === undefined) {
+      body.collection = 'favourites'
+    }
+
+    // I'm hoping this is an edge-case. Can't say for sure if devs should allow users to functionally
+    // access this endpoint without doing SOME checks first. Still, the guards are in place.
+    if (meta?.nick === null || meta === undefined) {
+      throw new Error('Un-authenticated users cannot have favs')
+    }
+
+    /** This piece of code operated within the given request context, and does
+     * not know or understand which collections in the 'collections' object are
+     * supposed to be collections of favourites.
+     *
+     * There is therefore nothing except developer oversight to prevent collections
+     * of entites from being conflated.
+     *
+     * _metaKey is the key in the collections object that we want to mutate
+     * [collectionScope][_metaKey] is the general bucket of favourites, for example.
+     * ['areaCollections']['favourites'] is the general bucket of favourites, for example.
+     *
+     * reqKey can be considered the entity type.
+     */
+    function addRemoveFavourite (
+      reqKey: keyof typeof body,
+      collectionScope: keyof ReifiedFavouriteCollections,
+      metaKey: string
+    ): void {
+      // ensure that an id has been supplied for this entity type
+      const targetEntity = body[reqKey]
+      if (targetEntity === undefined) return
+
+      // we mutate as a set, but store as an array
+      const favs: Set<string> = new Set(collections[collectionScope].get(metaKey))
+      const op = req.method === 'DELETE'
+        ? (id: string) => favs.delete(id)
+        : (id: string) => favs.add(id)
+
+      // Rest params may be lists, we can handle that easily
+      if (typeof targetEntity === 'string') {
+        op(targetEntity)
+      } else {
+        targetEntity.forEach((id: string) => {
+          op(id)
+        })
+      }
+
+      collections[collectionScope].set(metaKey, Array.from(favs))
+    }
+
+    // The two entities we support right now are Climbs and Areas
+    // Just as an intellectual exercise, we could do the following for... media or something:
+    // addToFavs('mediaId', 'favMedia')
+    if (typeof body.collection === 'string') {
+      addRemoveFavourite('climbId', 'climbCollections', body.collection)
+      addRemoveFavourite('areaId', 'areaCollections', body.collection)
+    } else {
+      body.collection.forEach((targetCollection) => {
+        addRemoveFavourite('climbId', 'climbCollections', targetCollection)
+        addRemoveFavourite('areaId', 'areaCollections', targetCollection)
+      })
+    }
+
+    // make the meta collections object reflect the new one
+    // TS should flag if we make any massive mistake here
+    meta.collections = {
+      climbCollections: Object.fromEntries(collections.climbCollections),
+      areaCollections: Object.fromEntries(collections.areaCollections)
+    }
+
+    // Commit the changes to the user's metadata
+    await metadataClient.updateUserMetadata(meta)
+    res.status(200).end()
+  } catch (e) {
+    // Could not add this climb to the user's favourites.
+    // Almost certainly a programmer error.
+    res.status(500).json({ error: e.message })
+  }
+}
+
+export default withAuth(handler)
