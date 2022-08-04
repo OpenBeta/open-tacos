41fc82d99cb274f4c204a2007de8f358b2d4de79
Viet Nguyen <3805254+vnugent@users.noreply.github.com>
2022-08-04 20:22:00 +0200

    Reduce load on Graph API server due to SSR of user pages (#478)
    * Reduce load on Graph API server due to SSR of user pages
    * Add placeholders to profile page

--- src/components/users/PublicProfile.tsx
+++ src/components/users/PublicProfile.tsx
@@ -1,5 +1,6 @@
-import { useCallback } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import Link from 'next/link'
+import ContentLoader from 'react-content-loader'
 
 import { IUserProfile } from '../../js/types/User'
 import EditProfileButton from './EditProfileButton'
@@ -9,21 +10,33 @@ interface PublicProfileProps {
   onClick?: () => void
 }
 
-export default function PublicProfile ({ userProfile }: PublicProfileProps): JSX.Element {
-  const { name, nick, avatar, bio, website } = userProfile
+export default function PublicProfile ({ userProfile: initialUserProfile }: PublicProfileProps): JSX.Element {
+  const [userProfile, setUserProfile] = useState<IUserProfile|null>(initialUserProfile)
+
+  useEffect(() => {
+    setUserProfile(initialUserProfile)
+  }, [initialUserProfile])
+
+  const { name, nick, avatar, bio, website } = userProfile ?? {}
   let websiteWithScheme: string | null = null
   if (website != null) {
     websiteWithScheme = website.startsWith('http') ? website : `//${website}`
   }
   return (
     <section className='mx-auto max-w-screen-sm px-4 md:px-0 md:grid md:grid-cols-3'>
-      <div className='hidden md:block grayscale pr-5'>
-        <img className='object-scale-down w-24 h-24 rounded-full hue-rotate-15' src={avatar} />
+      <div className='hidden md:block pr-5'>
+        {avatar == null
+          ? <AvatarPlaceholder uniqueKey={2} />
+          : <img className='grayscale  object-scale-down w-24 h-24 rounded-full' src={avatar} />}
       </div>
-      <div className='md:col-span-2 text-medium text-primary'>
+      <div className='md:col-span-2 text-medium text-primary '>
+        {nick == null && <TextPlaceholder uniqueKey={123} />}
+
         <div className='flex flex-row items-center'>
-          <div className='text-2xl font-bold mr-4'>{nick}</div>
-          <EditProfileButton ownerProfile={userProfile} />
+          <div className='text-2xl font-bold mr-4'>
+            {nick}
+          </div>
+          <EditProfileButton ownerProfile={initialUserProfile} />
         </div>
         <div className='mt-6 text-lg font-semibold'>{name}</div>
         <div className=''>{bio}</div>
@@ -42,6 +55,36 @@ export default function PublicProfile ({ userProfile }: PublicProfileProps): JSX
     </section>
   )
 }
+
+const AvatarPlaceholder = (props): JSX.Element => (
+  <ContentLoader
+    uniqueKey={props.uniqueKey}
+    height={96}
+    speed={0}
+    backgroundColor='rgb(209 213 219)'
+    viewBox='0 0 96 96'
+    {...props}
+  >
+    <circle cx='48' cy='48' r='48' />
+  </ContentLoader>
+)
+
+const TextPlaceholder = (props): JSX.Element => (
+  <ContentLoader
+    uniqueKey={props.uniqueKey}
+    height={108}
+    speed={0}
+    backgroundColor='rgb(209 213 219)'
+    viewBox='0 0 200 108'
+    {...props}
+  >
+    <rect x='0' y='8' rx='2' ry='2' width='120' height='18' />
+    <rect x='0' y='60' rx='2' ry='2' width='80' height='18' />
+    <rect x='0' y='90' rx='2' ry='2' width='200' height='12' />
+  </ContentLoader>
+)
+
+// export const PublicProfilePlaceHolder = (): JSX.Element => (<div className='mx-auto max-w-screen-sm px-4 md:px-0 md:grid md:grid-cols-3' />)
 
 /**
  * Remove leading http(s):// and trailing /
