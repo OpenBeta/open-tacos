fc3946adc83c77ade7dd11f966350333f69f99de
Downster <b.downing.code@gmail.com>
2022-08-04 10:47:09 -0700

    Added modal

--- src/components/users/ImportFromMtnProj.tsx
+++ src/components/users/ImportFromMtnProj.tsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
-
-interface ImportFromMtnProjProps {
-  modal?: boolean
-}
+import { Fragment, useState } from 'react'
+import { Transition } from '@headlessui/react'
+import { DocumentDownloadIcon } from '@heroicons/react/outline'
+import { XIcon } from '@heroicons/react/solid'
 
 // regex: ^\d{9}\/ -- matches the first 9 digits and a slash
 
-function ImportFromMtnProj ({ modal }: ImportFromMtnProjProps): JSX.Element | null {
+function ImportFromMtnProj ({ }): JSX.Element | null {
   const [mpUID, setMPUID] = useState('')
+  const [show, setShow] = useState(true)
 
   async function getTicks (): Promise<void> {
     // get the ticks and add it to the users metadata
@@ -16,9 +16,9 @@ function ImportFromMtnProj ({ modal }: ImportFromMtnProjProps): JSX.Element | nu
       method: 'PUT',
       body: JSON.stringify(mpUID)
     })
-    const stuff = await ret.json()
-    console.log(stuff)
+    const collections = await ret.json()
     if (ret.status === 200) {
+      console.log(collections)
       // do the fetch from mtn project and store that data in the Auth0 metadata
     } else {
       console.log(ret)
@@ -29,15 +29,66 @@ function ImportFromMtnProj ({ modal }: ImportFromMtnProjProps): JSX.Element | nu
   // if the modal prop is true we want to render this component as a notification/modal
   // otherwise we want it to be a button
     <>
-      <input
-        value={mpUID}
-        onChange={(e) => setMPUID(e.target.value)}
-        className='border-2 border-black'
-      />
-      <button
-        className='inline-flex space-x-2 items-center cursor-pointer disabled:cursor-auto disabled:opacity-50 border  border-gray-600 rounded-md bg-ob-primary text-white drop-shadow-md hover:ring-2 px-2.5 py-1 text-base'
-        onClick={getTicks}
-      />
+      <div
+        aria-live='assertive'
+        className='fixed inset-0 flex items-end px-4 py-6 mt-24 pointer-events-none sm:p-6 sm:items-start'
+      >
+        <div className='w-full flex flex-col items-center space-y-4 sm:items-end'>
+          {/* Notification panel, dynamically insert this into the live region when it needs to be displayed */}
+          <Transition
+            show={show}
+            as={Fragment}
+            enter='transform ease-out duration-300 transition'
+            enterFrom='translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2'
+            enterTo='translate-y-6 opacity-100 sm:translate-x-0'
+            leave='transition ease-in duration-100'
+            leaveFrom='opacity-100'
+            leaveTo='opacity-0'
+          >
+            <div className='max-w-sm w-full bg-white shadow-lg rounded-lg pointer-events-auto ring-1 ring-black ring-opacity-5 overflow-hidden'>
+              <div className='p-4'>
+                <div className='flex items-start'>
+                  <div className='flex-shrink-0'>
+                    <DocumentDownloadIcon className='h-6 w-6 text-gray-400' aria-hidden='true' />
+                  </div>
+                  <div className='ml-3 w-0 flex-1 pt-0.5'>
+                    <p className='text-sm font-medium text-gray-900'>Import your ticks from Mtn Project</p>
+                    <p className='mt-1 text-sm text-gray-500'>
+                      Don't loose your progress, bring it over to Open Beta.
+                    </p>
+                    <div className='mt-3 flex space-x-7'>
+                      <button
+                        type='button'
+                        className='bg-white rounded-md text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+                      >
+                        Show me how
+                      </button>
+                      <button
+                        type='button'
+                        className='bg-white rounded-md text-sm font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+                      >
+                        Dismiss
+                      </button>
+                    </div>
+                  </div>
+                  <div className='ml-4 flex-shrink-0 flex'>
+                    <button
+                      type='button'
+                      className='bg-white rounded-md inline-flex text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+                      onClick={() => {
+                        setShow(false)
+                      }}
+                    >
+                      <span className='sr-only'>Close</span>
+                      <XIcon className='h-5 w-5' aria-hidden='true' />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Transition>
+        </div>
+      </div>
     </>
   )
 }
