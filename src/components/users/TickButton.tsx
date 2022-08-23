3e78f271999f9485475d12c25d188dc533049256
Downster <b.downing.code@gmail.com>
2022-08-23 15:18:18 -0700

    message

--- src/components/users/TickButton.tsx
+++ src/components/users/TickButton.tsx
@@ -1,6 +1,5 @@
 import { signIn, useSession } from 'next-auth/react'
 import React, { useEffect, useState } from 'react'
-import { APIFavouriteCollections } from '../../pages/api/user/fav'
 import TickForm from './TickForm'
 
 interface Props {
@@ -23,69 +22,20 @@ function NoLogin (): JSX.Element {
   )
 }
 
-/** Assuming that there is a generic 'favourites' collection for the time being. */
-const favCollection = 'favourites'
 
-/** Built for rapid single-instancing. This component payloads its own network requests
- * so don't go making many instances without hoisting the requests into a parent.
- */
+
 export default function TickButton ({ climbId, areaId, name, grade }: Props): JSX.Element | null {
   const [loading, setLoading] = useState(false)
   const [isTicked, setIsTicked] = useState<boolean>(false)
-  const [open, setOpen] = useState(true)
+  const [open, setOpen] = useState(false)
   const session = useSession()
 
   useEffect(() => {
-    setLoading(true)
-    fetch('/api/user/ticks')
-      .then(async res => await res.json())
-      .then((collections: APIFavouriteCollections) => {
-        if (climbId !== undefined) {
-          const f = collections.climbCollections[favCollection]
-          if (f === undefined) {
-            setIsTicked(false)
-            return
-          }
+    //query graphQL by climbID and userID and see if there is already a tick
+    //if there is
+        //set it to is ticked
+  })
 
-          setIsTicked(f.includes(climbId))
-          return // guard block
-        }
-
-        if (areaId !== undefined) {
-          const f = collections.areaCollections[favCollection]
-          if (f === undefined) {
-            setIsTicked(false)
-            return
-          }
-
-          setIsTicked(f.includes(areaId))
-          // guard block
-        }
-      })
-      .catch(console.error)
-      .finally(() => {
-        setLoading(false)
-      })
-  }, [climbId, areaId])
-
-  const toggle = (): void => {
-    // open modal to input tick data
-    setOpen(true)
-    // let method = 'PUT'
-    // if (isTicked) {
-    //   method = 'DELETE'
-    // }
-
-    // fetch('/api/user/ticks', {
-    //   method: method,
-    //   body: JSON.stringify({ climbId, areaId, collection: favCollection })
-    // })
-    //   .then(() => setIsTicked(!isTicked))
-    //   .catch(err => console.error({ err }))
-    //   .finally(() => {
-    //     setLoading(false)
-    //   })
-  }
 
   // If there is some kind of programming error / user is un-authenticated we render the default
   // interaction-devoid button
@@ -97,7 +47,7 @@ export default function TickButton ({ climbId, areaId, name, grade }: Props): JS
     <>
       <button
         disabled={loading}
-        onClick={toggle}
+        onClick={() => setOpen(true)}
         className='mt-2 text-center p-2 border-2 rounded-xl border-ob-primary transition
         text-ob-primary hover:bg-ob-primary hover:ring hover:ring-ob-primary ring-offset-2
         hover:text-white w-64 font-bold'
@@ -108,7 +58,7 @@ export default function TickButton ({ climbId, areaId, name, grade }: Props): JS
               Working on it...
             </span>
             )
-          : (!isTicked ? 'Tick this climb' : 'Remove tick')}
+          : (!isTicked ? 'Tick this climb' : 'Tick again')}
       </button>
       <TickForm open={open} setOpen={setOpen} climbId={climbId} name={name} grade={grade} />
     </>
