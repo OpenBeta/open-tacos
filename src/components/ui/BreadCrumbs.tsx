37c96464df7eccf27c8d26356e8851cc6b09bc03
CocoisBuggy <g.colinharley@gmail.com>
2022-08-23 13:05:42 +0200

    Use hero icon package rather than JSX (duh)

--- src/components/ui/BreadCrumbs.tsx
+++ src/components/ui/BreadCrumbs.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 import { sanitizeName } from '../../js/utils'
+import { LocationMarkerIcon } from '@heroicons/react/outline'
 /**
  * Turn each element of `pathTokens` to a gatsby-link.
  *
@@ -23,10 +24,7 @@ interface BreakCrumbsProps {
 function BreadCrumbs ({ pathTokens, ancestors, isClimbPage = false }: BreakCrumbsProps): JSX.Element {
   return (
     <div aria-label='area-breadcrumbs' className='flex-wrap flex gap-2 text-sm items-center'>
-      <svg xmlns='http://www.w3.org/2000/svg' className='h-5 w-5 text-ob-primary' fill='none' viewBox='0 0 24 24' stroke='currentColor' strokeWidth={2}>
-        <path strokeLinecap='round' strokeLinejoin='round' d='M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z' />
-        <path strokeLinecap='round' strokeLinejoin='round' d='M15 11a3 3 0 11-6 0 3 3 0 016 0z' />
-      </svg>
+      <LocationMarkerIcon className='text-ob-primary w-5 h-5' />
 
       <Link href='/'>
         <a className='hover:underline hover:text-base-content text-base-300'>Home</a>
