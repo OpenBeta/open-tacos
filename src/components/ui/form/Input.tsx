104f5c1e26efda68f45a1c571c7682de61c503a5
viet nguyen <vietnguyen@noreply>
2022-09-04 11:12:12 +0200

    Input unit test

--- src/components/ui/form/Input.tsx
+++ src/components/ui/form/Input.tsx
@@ -3,28 +3,33 @@ import { useFormContext } from 'react-hook-form'
 interface InputProps {
   label: string
   name: string
-  placeholder: string
-  rules: any
-  className: string
+  placeholder?: string
+  rules?: any
+  className?: string
+  helper?: string | JSX.Element
 }
 
-export default function Input ({ label, name, rules, placeholder, className }: InputProps): JSX.Element {
+export default function Input ({ label, name, rules, placeholder = '', className, helper }: InputProps): JSX.Element {
   const { register, formState: { errors } } = useFormContext()
   const inputProps = register(name, rules)
   return (
     <div className='form-control'>
-      <label className='label'>
+      <label className='label' htmlFor={name}>
         <span className='label-text font-semibold'>{label}</span>
       </label>
       <input
+        id={name}
         {...inputProps}
         type='text'
         placeholder={placeholder}
         className={className}
+        aria-label={label}
+        aria-describedby={`${name}-helper`}
       />
-      <label className='label'>
+      <label className='label' id={`${name}-helper`} htmlFor={name}>
         {errors?.[name]?.message != null &&
            (<span className='label-text-alt text-error'>{errors?.[name]?.message as string}</span>)}
+        {((errors?.[name]) == null) && helper}
       </label>
     </div>
   )
--- src/components/ui/form/__tests__/RadioGroup.tsx
+++ src/components/ui/form/__tests__/RadioGroup.tsx
@@ -1,37 +1,40 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render, screen, renderHook } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import { useForm, FormProvider } from 'react-hook-form'
 
+import { Form } from './FormHelper'
 import RadioGroup from '../RadioGroup'
 
 test('RadioGroup can change state', async () => {
   const user = userEvent.setup()
-
-  const { result } = renderHook(() => useForm({ defaultValues: { testRadioGroup: '1' } }))
-  const { handleSubmit } = result.current
   const submitFn = jest.fn()
+  const defaultValues = { testRadioGroup: '1' }
   render(
-    <FormProvider {...result.current}>
-      <form onSubmit={handleSubmit(submitFn)}>
-        <RadioGroup
-          groupLabel='Test group'
-          name='testRadioGroup'
-          labels={['one', 'two']}
-          values={['1', '2']}
-        />
-        <button type='submit'>OK</button>
-      </form>
-    </FormProvider>
+    <Form
+      onSubmit={submitFn}
+      defaultValues={defaultValues}
+    >
+      <RadioGroup
+        groupLabel='Test group'
+        name='testRadioGroup'
+        labels={['one', 'two']}
+        values={['1', '2']}
+      />
+      <button type='submit'>OK</button>
+    </Form>
   )
 
   await user.click(screen.getByRole('button'))
 
   expect(submitFn).toHaveBeenCalledTimes(1)
-  expect(submitFn).toBeCalledWith({ testRadioGroup: '1' }, expect.anything())
+  expect(submitFn).toBeCalledWith(
+    { testRadioGroup: '1' },
+    expect.anything()) // we don't care about form 'ref' param but we have to ack it
 
+  // select the other option
   await user.click(screen.getByLabelText('two'))
+  // submit again
   await user.click(screen.getByRole('button'))
 
   expect(submitFn).toBeCalledWith({ testRadioGroup: '2' }, expect.anything())
