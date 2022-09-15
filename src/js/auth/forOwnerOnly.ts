880f942aa80a3aaf66ecb51363856fe5473b2c96
Viet Nguyen <3805254+vnugent@users.noreply.github.com>
2022-09-15 22:49:49 +0200

    Don't show api key copy icon if not page owner (#534)

--- src/js/auth/forOwnerOnly.ts
+++ src/js/auth/forOwnerOnly.ts
@@ -4,12 +4,14 @@ import React from 'react'
 import { WithOwnerProfile } from '../types/User'
 import usePermissions from '../hooks/auth/usePermissions'
 
+type Fn<P> = (props: P) => JSX.Element | null
 /**
  * Higher-order function to conditionally render a UI component
  * if the user currently authenticated is the same as the component owner.
  * @param Component to be rendered
  */
-const forOwnerOnly = <P extends WithOwnerProfile>(Component: (props: P) => JSX.Element): (props: P) => React.FunctionComponentElement<P> | null => {
+const forOwnerOnly = <P extends WithOwnerProfile>(Component: Fn<P>): (props: P) => React.FunctionComponentElement<P> | null => {
+  if (Component == null) return () => null
   const AuthenticatedFC = (props: P): null | React.FunctionComponentElement<P> => {
     const { isAuthorized } = usePermissions({ ownerProfileOnPage: props.ownerProfile })
     if (isAuthorized) {
