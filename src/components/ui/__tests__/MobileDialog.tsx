c2336844336089da3efd87917da80bc0ef6a02f4
viet nguyen <vietnguyen@noreply>
2022-09-17 20:21:33 +0200

    Unit test

---
+++ src/components/ui/__tests__/MobileDialog.tsx
@@ -0,0 +1,36 @@
+import '@testing-library/jest-dom/extend-expect'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+import { MobileDialog, DialogContent, DialogTrigger } from '../MobileDialog'
+
+test('MobileDialog smoke test', async () => {
+  const user = userEvent.setup()
+  render(
+    <div style={{ pointerEvents: 'auto' }}>
+      <MobileDialog modal>
+        <DialogTrigger className='btn'>
+          Add
+        </DialogTrigger>
+        <DialogContent title='Sunshine'>
+          <button>Submit</button>
+        </DialogContent>
+      </MobileDialog>
+    </div>)
+
+  // Dialog title shouldn't be there
+  expect(screen.queryByLabelText('Sunshine')).toBeNull()
+
+  await user.click(screen.getByRole('button', { name: 'Add' }))
+
+  // does dialog content exist?
+  expect(screen.queryByLabelText('Sunshine')).not.toBeNull()
+  expect(screen.queryByRole('button', { name: 'Submit' })).not.toBeNull()
+  expect(screen.queryByRole('button', { name: 'Close' })).not.toBeNull()
+
+  // close dialog
+  await user.click(screen.getByRole('button', { name: 'Close' }))
+
+  // title shouldn't be there
+  expect(screen.queryByLabelText('Sunshine')).toBeNull()
+})
