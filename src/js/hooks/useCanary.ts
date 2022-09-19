802814b9ce619edd27ad51600df64f9e8d14c42b
Viet Nguyen <3805254+vnugent@users.noreply.github.com>
2022-09-19 16:49:04 +0200

    Persist canary flag longer (in local storage) (#543)
    Co-authored-by: viet nguyen <vietnguyen@noreply>

--- src/js/hooks/useCanary.ts
+++ src/js/hooks/useCanary.ts
@@ -5,19 +5,19 @@ import { useRouter } from 'next/router'
  * @returns true if user requests a page with ?next=true
  */
 export default function useCanary (): boolean {
-  if (typeof sessionStorage === 'undefined') {
+  if (typeof localStorage === 'undefined') {
     return false
   }
   const router = useRouter()
   const isNext = router.query.next // url contains ?next=true
   if (typeof isNext === 'string') {
     if (isNext === 'true') {
-      sessionStorage.setItem('canary', 'true')
+      localStorage.setItem('canary', 'true')
       return true
     } else {
-      sessionStorage.setItem('canary', 'false')
+      localStorage.setItem('canary', 'false')
       return false
     }
   }
-  return sessionStorage.getItem('canary') === 'true'
+  return localStorage.getItem('canary') === 'true'
 }
