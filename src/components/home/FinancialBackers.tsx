44dc9412f981badd94f634a08aa82de83d0596b5
Marcos A Rosas <contactme@marcosarosas.dev>
2022-09-25 19:02:11 -0500

    Updated FinancialBackers to remove Duplicate donor

--- src/components/home/FinancialBackers.tsx
+++ src/components/home/FinancialBackers.tsx
@@ -1,18 +1,23 @@
-import { FinancialBackerAccountType } from '../../js/types'
 import BackerCard from '../ui/BackerCard'
+import { FinancialBackerAccountType } from '../../js/types'
+import { uniq } from 'underscore'
 
 export interface FinancialBackersProps {
   donors: FinancialBackerAccountType[]
   totalRaised: string
 }
 
-export default function FinancialBackers ({ donors, totalRaised }: FinancialBackersProps): JSX.Element {
+export default function FinancialBackers({ donors, totalRaised }: FinancialBackersProps): JSX.Element {
+
+
+  const uniqDonors = uniq(donors, (donor) => donor.account.name)  // Using the underscore library to get unique donors, based on their name
+
   return (
     <div className='text-primary text-center pb-10'>
       <h3>Thanks to our financial backers we've raised ${totalRaised}</h3>
       <div>
         <div className='grid grid-cols-4 gap-4 columns-xs'>
-          {donors?.map(donor => {
+          {uniqDonors?.map(donor => {
             const name = donor.account.name
             const imageUrl = donor.account.imageUrl
 
