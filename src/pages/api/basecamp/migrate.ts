e7854e175730d9c46b3eeb0df596773d7c7bc09e
Viet Nguyen <3805254+vnugent@users.noreply.github.com>
2022-09-30 17:59:45 +0200

    Migrate users to email/password (#558)
    * Admin tool to migrate users to email/password

---
+++ src/pages/api/basecamp/migrate.ts
@@ -0,0 +1,49 @@
+import { NextApiHandler } from 'next'
+import { getSession } from 'next-auth/react'
+import withAuth from '../withAuth'
+import { CreateUserData } from 'auth0'
+import { customAlphabet } from 'nanoid'
+import { nolookalikesSafe } from 'nanoid-dictionary'
+
+import { auth0ManagementClient } from '../../../js/auth/ManagementClient'
+
+const handler: NextApiHandler<any> = async (req, res) => {
+  try {
+    const session = await getSession({ req })
+    if (session?.user.metadata?.roles?.includes('user_admin') ?? false) {
+      const userId = req.query?.id as string
+      if (userId == null) throw new Error('Invalid user id')
+
+      const oldUser = await auth0ManagementClient.getUser({ id: userId })
+      if (oldUser?.email == null || oldUser?.user_metadata == null) {
+        throw new Error('Missing email/user_metadata in Auth0.')
+      }
+
+      const newUserMetadata = Object.assign({}, oldUser.user_metadata)
+      delete newUserMetadata.migrated
+      delete newUserMetadata.migratedDate
+
+      const newUserData: CreateUserData = {
+        connection: 'Username-Password-Authentication',
+        email: oldUser.email,
+        user_metadata: newUserMetadata,
+        email_verified: true,
+        password: safeRandomString()
+      }
+
+      await auth0ManagementClient.createUser(newUserData)
+      oldUser.user_metadata.migratedDate = new Date(Date.now()).toISOString()
+
+      await auth0ManagementClient.updateUserMetadata({ id: userId }, oldUser.user_metadata)
+      res.json({ message: 'OK' })
+    } else {
+      res.status(401).json({ message: 'Migration failed: user not authorized', errorCode: 3 })
+    }
+  } catch (e) {
+    res.status(503).json({ message: `Unexpected error: ${e.message as string}`, errorCode: 2 })
+  }
+}
+
+const safeRandomString = customAlphabet(nolookalikesSafe, 20)
+
+export default withAuth(handler)
--- src/pages/api/basecamp/users.ts
+++ src/pages/api/basecamp/users.ts
@@ -1,11 +1,17 @@
 import { NextApiHandler } from 'next'
+import { getSession } from 'next-auth/react'
 import withAuth from '../withAuth'
 import { getAllUsersMetadata } from '../../../js/auth/ManagementClient'
 
 const handler: NextApiHandler<any> = async (req, res) => {
   try {
-    const users = await getAllUsersMetadata(true)
-    res.json(users)
+    const session = await getSession({ req })
+    if (session?.user.metadata?.roles?.includes('user_admin') ?? false) {
+      const users = await getAllUsersMetadata(true)
+      res.json(users)
+    } else {
+      res.status(401).end()
+    }
   } catch (e) {
     res.status(200).redirect(401, '/')
   }
