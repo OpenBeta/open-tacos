a4ff2ec209585140d773c260694315f37673af7a
viet nguyen <vietnguyen@noreply>
2022-10-24 11:45:34 -0500

    New email verification holding page

--- src/pages/auth/verify-request.tsx
+++ src/pages/auth/verify-request.tsx
@@ -1,39 +1,75 @@
-import { MailIcon } from '@heroicons/react/outline'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import { useSession, signIn } from 'next-auth/react'
+import { MailIcon, ArrowSmRightIcon } from '@heroicons/react/outline'
 import { useRouter } from 'next/router'
-import { useSession } from 'next-auth/react'
 import axios from 'axios'
+import useSWR from 'swr'
 
-function SignInPage (): JSX.Element {
-  const router = useRouter()
+import SeoTags from '../../components/SeoTags'
+
+interface TokenHelperType {
+  auth0UserId: string
+}
+
+function VerifyEmailPendingPage (): JSX.Element {
   const session = useSession()
+
+  const [sending, setSending] = useState(false)
+  const router = useRouter()
   const token = router.query?.session_token as string
+
+  const url = `/api/user/emailVerification?token=${token}`
+  const { error } = useSWR<TokenHelperType>(token != null ? url : null, fetcher, { refreshInterval: 0 })
+
+  const onClickHandler = async (): Promise<void> => {
+    if (error == null) {
+      setSending(true)
+      await axios.post(url)
+      // poor man deboucing
+      // eslint-disable-next-line
+      await new Promise(r => setTimeout(r, 3000))
+      setSending(false)
+    }
+  }
+
   useEffect(() => {
-    console.log('#Session token', token)
-    const decodeToken = async (): Promise<any> => {
-      // const key = await getDerivedEncryptionKey('top secret')
-      // const { payload } = await jose.jwtVerify(token, 'top secret')
-      const res = await axios.get('/api/user/tokenHelper?token=' + token)
-      console.log('#token', res.data)
-      return res.data
+    // just in case the user has signed in in another browser window.
+    // Though `useSession()` refreshes periodically?
+    if (session.status === 'authenticated') {
+      void router.replace('/')
     }
-
-    if (token != null) {
-      void decodeToken()
-    }
-  })
-
-  console.log('#Session', session)
+  }, [session.status, router])
 
   return (
-    <div className='mt-8 max-w-sm mx-auto alert alert-info'>
-      <div className='flex items-center gap-4'>
-        <MailIcon className='w-16 h-16' />
-        <span>We've sent you a verification email. (Please also check your spam folder)</span>
+    <>
+      <SeoTags
+        title='Email verification pending - OpenBeta'
+      />
+      <div className='bg-base-content h-screen pt-8'>
+        <section className='bg-base-100 max-w-sm mx-auto px-4 py-12 rounded-box'>
+          <div className='flex flex-col gap-4 items-center'>
+            <MailIcon className='w-8 h-8 animate-bounce stroke-accent' />
+            <hr className='w-full py-1 border-base-300' />
+            <div className='text-center'>Look for a verification email in your inbox. <strong>Don't forget to check your spam folder </strong>🙂</div>
+            <div className='mt-4 flex flex-col gap-4 items-center w-full'>
+              {error == null &&
+                <button
+                  disabled={sending}
+                  className='btn btn-outline' onClick={onClickHandler}
+                >
+                  Resend verification email
+                </button>}
+              <button className='btn btn-ghost btn-sm text-sm text-base-300' onClick={() => { void signIn('auth0') }}>
+                Already verified? Login <ArrowSmRightIcon className='ml-2 w-5 h-5' />
+              </button>
+            </div>
+          </div>
+        </section>
       </div>
-
-    </div>
+    </>
   )
 }
 
-export default SignInPage
+export default VerifyEmailPendingPage
+
+const fetcher = async (url: string): Promise<any> => (await axios.get(url)).data
