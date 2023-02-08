f146d17115975ad7efcf57eaf28b7bbb298dc617
Junaid <95287515+theonlyjunaid@users.noreply.github.com>
2023-02-08 21:26:17 +0530

    image viewer implimented

--- src/components/media/PhotoGalleryModal.tsx
+++ src/components/media/PhotoGalleryModal.tsx
@@ -1,6 +1,7 @@
-import React, { Dispatch, SetStateAction } from 'react'
+import React, { Dispatch, SetStateAction,useState } from 'react'
 import { userMediaStore } from '../../js/stores/media'
 import { ResponsiveImage2 } from './slideshow/ResponsiveImage'
+import ResponsiveImage  from "./slideshow/ResponsiveImage";
 import { MobileDialog, DialogContent } from '../ui/MobileDialog'
 
 interface PhotoGalleryModalProps {
@@ -12,6 +13,7 @@ interface PhotoGalleryModalProps {
  * @param setShowPhotoGallery A setState action that toggles the photo gallery modal on click.
  */
 const PhotoGalleryModal = ({ setShowPhotoGalleryModal }: PhotoGalleryModalProps): JSX.Element => {
+    const [ShowImage, setShowImage] = useState('')
   const photoList = userMediaStore.use.photoList()
   return (
     <MobileDialog modal open onOpenChange={() => setShowPhotoGalleryModal(false)}>
@@ -23,6 +25,9 @@ const PhotoGalleryModal = ({ setShowPhotoGalleryModal }: PhotoGalleryModalProps)
             const { height, width } = mediaInfo?.meta
             return (
               <div
+                 onClick={() => {
+                  setShowImage(element.mediaUrl), console.log(element.mediaUrl);
+                }}
                 key={element.mediaUrl}
                 className='overflow-hidden mt-0 mb-2 lg:mb-4 hover:brightness-75 break-inside-avoid-column break-inside-avoid relative block rounded-md'
               >
@@ -37,6 +42,45 @@ const PhotoGalleryModal = ({ setShowPhotoGalleryModal }: PhotoGalleryModalProps)
           }
           )}
         </div>
+           {
+        ShowImage ? ( 
+          <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75">
+            <div className="relative w-full h-full">
+              <div className="absolute top-2 right-2 z-50">
+                <button
+                  onClick={() => setShowImage('')}
+                  className="bg-white hover:bg-gray-500 bg-opacity-50 rounded-full  duration-300 ease-in-out p-2"
+                >
+                  <svg
+
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6 text-black"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
+              </div>
+              <div className="flex items-center justify-center h-full">
+
+          <ResponsiveImage
+            mediaUrl={ShowImage}
+            isHero={false}
+          />
+
+              </div>
+            </div>
+          </div>
+        ) : null
+
+     }
         <div className='h-16'>&nbsp;</div>
       </DialogContent>
     </MobileDialog>
