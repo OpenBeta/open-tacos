40e18201f90aaeaa6757648b4dc4767c8fe8e0cd
Junaid <95287515+theonlyjunaid@users.noreply.github.com>
2023-02-09 21:02:42 +0530

    issue #685 solved image viewer added in gallery section (#689)
    * image viewer implimented
    * Update PhotoGalleryModal.tsx
    * x icon added

--- src/components/media/PhotoGalleryModal.tsx
+++ src/components/media/PhotoGalleryModal.tsx
@@ -1,7 +1,8 @@
-import React, { Dispatch, SetStateAction } from 'react'
+import React, { Dispatch, SetStateAction, useState } from 'react'
 import { userMediaStore } from '../../js/stores/media'
-import { ResponsiveImage2 } from './slideshow/ResponsiveImage'
+import ResponsiveImage, { ResponsiveImage2 } from './slideshow/ResponsiveImage'
 import { MobileDialog, DialogContent } from '../ui/MobileDialog'
+import { XMarkIcon } from '@heroicons/react/24/outline'
 
 interface PhotoGalleryModalProps {
   setShowPhotoGalleryModal: Dispatch<SetStateAction<boolean>>
@@ -11,20 +12,28 @@ interface PhotoGalleryModalProps {
  * A reusable popup alert
  * @param setShowPhotoGallery A setState action that toggles the photo gallery modal on click.
  */
-const PhotoGalleryModal = ({ setShowPhotoGalleryModal }: PhotoGalleryModalProps): JSX.Element => {
+const PhotoGalleryModal = ({
+  setShowPhotoGalleryModal
+}: PhotoGalleryModalProps): JSX.Element => {
+  const [ShowImage, setShowImage] = useState('')
   const photoList = userMediaStore.use.photoList()
   return (
-    <MobileDialog modal open onOpenChange={() => setShowPhotoGalleryModal(false)}>
+    <MobileDialog
+      modal
+      open
+      onOpenChange={() => setShowPhotoGalleryModal(false)}
+    >
       <DialogContent fullScreen title='Gallery'>
         <div className='px-0 lg:px-4 mt-20 relative columns-2 md:columns-3 lg:columns-4 xl:columns-5 gap-2 lg:gap-4'>
-          {photoList.map(element => {
+          {photoList.map((element) => {
             const mediaInfo = element?.mediaInfo
             if (mediaInfo == null) return null
             const { height, width } = mediaInfo?.meta
             return (
               <div
+                onClick={() => setShowImage(element.mediaUrl)}
                 key={element.mediaUrl}
-                className='overflow-hidden mt-0 mb-2 lg:mb-4 hover:brightness-75 break-inside-avoid-column break-inside-avoid relative block rounded-md'
+                className='overflow-hidden mt-0 mb-2 lg:mb-4 hover:brightness-75 break-inside-avoid-column cursor-pointer break-inside-avoid relative block rounded-md'
               >
                 <ResponsiveImage2
                   naturalWidth={width}
@@ -34,10 +43,31 @@ const PhotoGalleryModal = ({ setShowPhotoGalleryModal }: PhotoGalleryModalProps)
                 />
               </div>
             )
-          }
-          )}
+          })}
         </div>
-        <div className='h-16'>&nbsp;</div>
+
+        {
+        ShowImage !== ''
+          ? (
+            <div className='fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75'>
+              <div className='relative w-full h-full'>
+                <div className='absolute top-2 right-2 z-50'>
+                  <button
+                    onClick={() => setShowImage('')}
+                    className='bg-white hover:bg-gray-500 bg-opacity-50 rounded-full  duration-300 ease-in-out p-2'
+                  >
+                    <XMarkIcon className='w-6 h-6' />
+                  </button>
+                </div>
+                <div className='flex items-center justify-center h-full'>
+                  <ResponsiveImage mediaUrl={ShowImage} isHero={false} />
+                </div>
+              </div>
+            </div>
+            )
+          : null
+        }
+
       </DialogContent>
     </MobileDialog>
   )
