eb79bc57f93a085638c189a6bd4bc451dfa03cd0
viet nguyen <vietnguyen@noreply>
2023-03-10 17:25:10 +0100

    extract LCO list to a separate file

--- src/components/lco/PageBanner.tsx
+++ src/components/lco/PageBanner.tsx
@@ -1,34 +1,22 @@
 import * as HoverCard from '@radix-ui/react-hover-card'
 import { ClimbType } from '../../js/types'
-
-interface LCOProfileType {
+import { LCO_LIST } from './data'
+export interface LCOProfileType {
+  /** Org unique id  */
   id: string
+  /** An array of area IDs under the org care */
   areaIdList: string[]
+  /** Official name */
   name: string
+  /** IG Url */
   instagram: string
+  /** Official website */
   website: string
+  /** Bad hardware report form url */
   report?: string
+  /** Donation url */
   donation?: string
 }
-
-const LCO_LIST: LCOProfileType[] = [
-  {
-    id: 'a791edd2-6a19-4f12-b69d-cbd499fed81f',
-    areaIdList: ['e519a674-a620-509c-9e86-a246f84a8e40'],
-    name: 'Boulder Climbing Community',
-    instagram: 'https://www.instagram.com/boulderclimbingcommunity/',
-    website: 'https://www.boulderclimbers.org/',
-    report: 'https://docs.google.com/forms/d/e/1FAIpQLSfRditFRHJIt7ayZ1SPet2gkl5wh7QF8DnoqRGf1kdzCgegQg/viewform',
-    donation: 'https://secure.givelively.org/donate/boulder-climbing-community/donation'
-  }, {
-    id: '8801b124-ab19-4b66-a292-738dc56e446a',
-    areaIdList: ['c80d3abd-beab-5467-a559-edee707f68bd'],
-    name: 'Portland Area Climbers Coalition',
-    instagram: 'https://www.instagram.com/portlandclimbing/',
-    website: 'https://www.oregonclimbers.org/',
-    report: 'https://forms.gle/BofUZFFhF6Ann1oZ7'
-  }
-]
 
 const findLCOs = (lcoList: LCOProfileType[], currentPageAncestors: string[]): LCOProfileType[] => {
   return lcoList.reduce<LCOProfileType[]>((acc, curr) => {
@@ -43,11 +31,15 @@ const isMyCrag = (ancestors: string[], myCrags: string[]): boolean => ancestors.
 
 type PageBannerProps = Pick<ClimbType, 'ancestors'>
 
+/**
+ * Display LCO banner if there is one.  An area may have multiple LCOs.
+ */
 export const PageBanner: React.FC<PageBannerProps> = ({ ancestors }) => {
   const orgs = findLCOs(LCO_LIST, ancestors)
+  if (orgs.length === 0) return null
   return (
     <div className='mt-8 bg-area-cue/20 p-6 rounded-box'>
-      <div className='mt-2 text-xs text-base-300'>Local climbing organizations</div>
+      <div className='mt-2 mb-1 text-xs text-base-300'>Local climbing organizations</div>
       <div className='flex flex-col'>
         {orgs.map(orgProfile => <IndividualBanner key={orgProfile.id} profile={orgProfile} />)}
       </div>
@@ -56,7 +48,7 @@ export const PageBanner: React.FC<PageBannerProps> = ({ ancestors }) => {
 }
 
 const IndividualBanner: React.FC<ContentProps> = ({ profile }) => (
-  <HoverCard.Root openDelay={400}>
+  <HoverCard.Root openDelay={300}>
     <HoverCard.Trigger>
       <ContentTrigger profile={profile} />
     </HoverCard.Trigger>
@@ -89,8 +81,8 @@ const Card: React.FC<ContentProps> = ({ profile }) => {
   const { name, website, instagram, report, donation } = profile
   return (
     <div className='card w-96 bg-secondary shadow-xl z-50 overflow-clip border-base-200'>
-      <div className='card-body'>
-        <h2 className='card-title mt-2 uppercase'>{name}</h2>
+      <div className='p-8 pt-10'>
+        <h2 className='card-title my-2 uppercase'>{name}</h2>
         <div className='flex gap-4'>
           <a className='badge badge-outline opacity-60' href={website} target='_blank' rel='noreferrer'>website</a>
           <a className='badge badge-outline opacity-60' href={instagram} target='_blank' rel='noreferrer'>instagram</a>
---
+++ src/components/lco/data.tsx
@@ -0,0 +1,26 @@
+import { LCOProfileType } from './PageBanner'
+
+export const LCO_LIST: LCOProfileType[] = [
+  {
+    id: 'a791edd2-6a19-4f12-b69d-cbd499fed81f',
+    areaIdList: ['e519a674-a620-509c-9e86-a246f84a8e40'],
+    name: 'Boulder Climbing Community',
+    instagram: 'https://www.instagram.com/boulderclimbingcommunity/',
+    website: 'https://www.boulderclimbers.org/',
+    report: 'https://docs.google.com/forms/d/e/1FAIpQLSfRditFRHJIt7ayZ1SPet2gkl5wh7QF8DnoqRGf1kdzCgegQg/viewform',
+    donation: 'https://secure.givelively.org/donate/boulder-climbing-community/donation'
+  }, {
+    id: '8801b124-ab19-4b66-a292-738dc56e446a',
+    areaIdList: [
+      'c80d3abd-beab-5467-a559-edee707f68bd', // Portland & The Gorges
+      '5049661c-2645-50ff-8014-3e7d2ccc9e85', // Salmon River Slab
+      'a51e21da-e030-55d8-bd9a-6b2f72cd231b', // Klinger
+      'a8c395e6-9bbd-53ab-ba22-43995142b470', // Beacon
+      '079f2530-9cd1-5d5c-9b98-3088064aba5e' // Ozone
+    ],
+    name: 'Portland Area Climbers Coalition',
+    instagram: 'https://www.instagram.com/portlandclimbing/',
+    website: 'https://www.oregonclimbers.org/',
+    report: 'https://forms.gle/BofUZFFhF6Ann1oZ7'
+  }
+]
