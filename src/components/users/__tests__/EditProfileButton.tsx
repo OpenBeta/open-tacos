6186577c04451e7914a74cd2a153b2b54945ca0a
clintonlunn <cpl09c@gmail.com>
2023-04-21 21:24:23 -0700

    add unit tests for EditProfileButton and forOwnerOnly

--- src/components/users/__tests__/EditProfileButton.tsx
+++ src/components/users/__tests__/EditProfileButton.tsx
@@ -1,11 +1,9 @@
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
+import usePermissions from '../../../js/hooks/auth/usePermissions'
 
-const mockedUseSession = jest.fn()
-
-jest.mock('next-auth/react', () => ({
-  useSession: mockedUseSession
-}))
+// Mock the usePermissions hook
+jest.mock('../../../js/hooks/auth/usePermissions')
 
 jest.requireMock('next-auth/react')
 
@@ -17,19 +15,33 @@ beforeAll(async () => {
 })
 
 test('EditProfileButton renders when the user has logged in', async () => {
-  mockedUseSession
-    .mockClear()
-    .mockReturnValue({ status: 'authenticated' })
-  render(<EditProfileButton />)
-  expect(mockedUseSession).toBeCalledTimes(1)
+  (usePermissions as jest.Mock).mockReturnValue({ isAuthorized: true })
+  render(<EditProfileButton loginsCount={5} />)
+  screen.debug()
   expect(screen.queryByRole('button')).toBeDefined()
 })
 
 test('EditProfileButton renders null when the user hasn\'t logged in', async () => {
-  mockedUseSession
-    .mockClear()
-    .mockReturnValue({ status: '' })
-  render(<EditProfileButton />)
-  expect(mockedUseSession).toBeCalledTimes(1)
+  (usePermissions as jest.Mock).mockReturnValue({ isAuthorized: false })
+
+  render(<EditProfileButton loginsCount={0} />)
   expect(screen.queryByRole('button')).toBeNull()
+})
+
+test('EditProfileButton renders the tooltip when loginsCount is 1', async () => {
+  (usePermissions as jest.Mock).mockReturnValue({ isAuthorized: true })
+  render(<EditProfileButton loginsCount={1} />)
+  const tooltip = screen.queryByText(
+    'It looks like this is your first time logging in, click here to change your username!'
+  )
+  expect(tooltip).toBeInTheDocument()
+})
+
+test('EditProfileButton does not render the tooltip when loginsCount is greater than 1', async () => {
+  (usePermissions as jest.Mock).mockReturnValue({ isAuthorized: true })
+  render(<EditProfileButton loginsCount={5} />)
+  const tooltip = screen.queryByText(
+    'It looks like this is your first time logging in, click here to change your username!'
+  )
+  expect(tooltip).not.toBeInTheDocument()
 })
