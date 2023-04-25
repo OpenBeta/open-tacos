dd3461898cac79af6c575f219faecf8db0a34aa9
Clinton Lunn <cpl09c@gmail.com>
2023-04-25 01:13:43 -0700

    adding unit test to forOwnerOnly #794 (#795)
    * adding unit test to forOwnerOnly #794
    
    * add description for Fn type that accepts generic param P
    
    * delete empty line

--- src/js/auth/forOwnerOnly.ts
+++ src/js/auth/forOwnerOnly.ts
@@ -1,10 +1,16 @@
 
 import React from 'react'
-
 import { WithOwnerProfile } from '../types/User'
 import usePermissions from '../hooks/auth/usePermissions'
 
+/**
+ * A function type that takes a set of props of type P and returns a JSX element or null.
+ * @template P - The type of the props passed to the function.
+ * @param {P} props - The props passed to the function.
+ * @returns {JSX.Element | null} - The JSX element returned by the function, or null.
+ */
 type Fn<P> = (props: P) => JSX.Element | null
+
 /**
  * Higher-order function to conditionally render a UI component
  * if the user currently authenticated is the same as the component owner.
