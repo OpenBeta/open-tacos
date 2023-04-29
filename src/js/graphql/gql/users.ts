749bd3d00cc8b9393685f0aded386b2ddfdec434
viet nguyen <vietnguyen@noreply>
2023-04-29 18:49:03 +0200

    WIP: profile & climb GQL should use the same media fragment

--- src/js/graphql/gql/users.ts
+++ src/js/graphql/gql/users.ts
@@ -1,22 +1,10 @@
 import { gql } from '@apollo/client'
-
+import { FRAGMENT_MEDIA_WITH_TAGS } from './tags'
 export const QUERY_USER_MEDIA = gql`
+  ${FRAGMENT_MEDIA_WITH_TAGS}
   query ($userUuid: ID!, $limit: Int) {
     getUserMedia(userUuid: $userUuid, limit: $limit ) {
-      mediaUrl
-      height
-      width
-      birthTime
-      climbTags {
-        name
-        id
-        type
-      }
-      areaTags {
-        id
-        name
-        type
-      }
+      ... MediaWithTagsFields
     }
   }
 `
