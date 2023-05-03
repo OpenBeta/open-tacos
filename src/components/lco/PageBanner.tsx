b76b33d942c299e32b4e6628f9667e3cae7c87af
Yun Ji <87448230+actuallyyun@users.noreply.github.com>
2023-05-03 14:48:48 +0200

    issue #763 LCO List Component based on new Figma design (#782)
    * feat:move lco banner from left to right
    
    * feat:if no lco found, show empty state
    
    * use custom background color
    
    * replace info icon svg with react component
    
    * add previous hover card implementation in comments
    
    * change LCO logo placeholder to UsersIcon
    
    * style:use de-emphasized color for empty lco state text
    
    * fix linting errors
    
    * remove tooltip
    
    * click on lco link opens a new tag
    
    * add tooltip to show learn more
    
    * fix linting errors

--- src/components/lco/PageBanner.tsx
+++ src/components/lco/PageBanner.tsx
@@ -1,6 +1,10 @@
-import * as HoverCard from '@radix-ui/react-hover-card'
+// import * as HoverCard from '@radix-ui/react-hover-card'
 import { ClimbType } from '../../js/types'
 import { LCO_LIST } from './data'
+import Tooltip from '../../components/ui/Tooltip'
+import { InformationCircleIcon } from '@heroicons/react/20/solid'
+import { UsersIcon } from '@heroicons/react/24/outline'
+
 export interface LCOProfileType {
   /** Org unique id  */
   id: string
@@ -18,7 +22,10 @@ export interface LCOProfileType {
   donation?: string
 }
 
-const findLCOs = (lcoList: LCOProfileType[], currentPageAncestors: string[]): LCOProfileType[] => {
+const findLCOs = (
+  lcoList: LCOProfileType[],
+  currentPageAncestors: string[]
+): LCOProfileType[] => {
   return lcoList.reduce<LCOProfileType[]>((acc, curr) => {
     if (isMyCrag(currentPageAncestors, curr.areaIdList)) {
       acc.push(curr)
@@ -27,7 +34,8 @@ const findLCOs = (lcoList: LCOProfileType[], currentPageAncestors: string[]): LC
   }, [])
 }
 
-const isMyCrag = (ancestors: string[], myCrags: string[]): boolean => ancestors.some(path => myCrags.some(myCragId => myCragId === path))
+const isMyCrag = (ancestors: string[], myCrags: string[]): boolean =>
+  ancestors.some((path) => myCrags.some((myCragId) => myCragId === path))
 
 type PageBannerProps = Pick<ClimbType, 'ancestors'>
 
@@ -36,62 +44,101 @@ type PageBannerProps = Pick<ClimbType, 'ancestors'>
  */
 export const PageBanner: React.FC<PageBannerProps> = ({ ancestors }) => {
   const orgs = findLCOs(LCO_LIST, ancestors)
-  if (orgs.length === 0) return null
   return (
-    <div className='mt-8 bg-area-cue/20 p-6 rounded-box'>
-      <div className='mt-2 mb-1 text-xs text-base-300'>Local climbing organizations</div>
-      <div className='flex flex-col'>
-        {orgs.map(orgProfile => <IndividualBanner key={orgProfile.id} profile={orgProfile} />)}
+    <div className='grid pt-6 pb-4 lg:pb-16 lg:pt-16'>
+      <div className='col-span-full flex justify-start items-center pb-6'>
+        <h3 className='mr-4'>Local climbing organizations</h3>
+        <Tooltip content={
+          <p>Learn more about our&nbsp;
+            <a href='https://openbeta.substack.com/p/openbeta-and-lcos' target='_blank' rel='noreferrer' className='underline'>
+              initiative
+            </a>.
+          </p>
+        }
+        >
+          <InformationCircleIcon className='h-6 w-6' />
+        </Tooltip>
+      </div>
+      <div>
+        {orgs.length === 0
+          ? (
+            <p className='italic text-base-content/60'>
+              No organizationa found for this area
+            </p>
+            )
+          : (
+              orgs.map((orgProfile) => (
+                <IndividualBanner key={orgProfile.id} profile={orgProfile} />
+              ))
+            )}
       </div>
     </div>
   )
 }
 
 const IndividualBanner: React.FC<ContentProps> = ({ profile }) => (
-  <HoverCard.Root openDelay={300}>
-    <HoverCard.Trigger>
-      <ContentTrigger profile={profile} />
-    </HoverCard.Trigger>
-    <HoverCard.Portal>
-      <HoverCard.Content sideOffset={10} side='top' className='z-40'>
-        <Card profile={profile} />
-      </HoverCard.Content>
-    </HoverCard.Portal>
-  </HoverCard.Root>
+  <>
+    <div className='sm:inline-block mr-6 mb-6'>
+      <div className='flex items-center bg-light hover:bg-on-hover pl-5 pr-7 rounded-2xl'>
+        <UsersIcon className='h-10 w-10' />
+        <div className='py-5  pl-4 overflow-hidden'>
+          <p className='text-base leading-6'>{profile.name}</p>
+          <p className='text-xs underline'>
+            <a href={profile.website} target='_blank' rel='noreferrer'>{profile.website}</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </>
 )
 
 interface ContentProps {
   profile: LCOProfileType
 }
-const ContentTrigger: React.FC<ContentProps> = ({ profile }) => {
-  const { name, website } = profile
-  return (
-    <a
-      className='block uppercase hover:underline font-medium'
-      href={website}
-      target='_blank'
-      rel='noreferrer'
-    >
-      {name}
-    </a>
-  )
-}
 
-const Card: React.FC<ContentProps> = ({ profile }) => {
-  const { name, website, instagram, report, donation } = profile
-  return (
-    <div className='card w-96 bg-secondary shadow-xl z-50 overflow-clip border-base-200'>
-      <div className='p-8 pt-10'>
-        <h2 className='card-title my-2 uppercase'>{name}</h2>
-        <div className='flex gap-4'>
-          <a className='badge badge-outline opacity-60' href={website} target='_blank' rel='noreferrer'>website</a>
-          <a className='badge badge-outline opacity-60' href={instagram} target='_blank' rel='noreferrer'>instagram</a>
-          {donation != null && <a className='badge opacity-60 px-4' href={donation} target='_blank' rel='noreferrer'>Donate</a>}
-        </div>
-      </div>
-      <div className='card-actions bg-base-100 flex justify-end p-4'>
-        <a className='btn btn-primary btn-sm opacity-80' href={report} target='_blank' rel='noreferrer'>Report bad bolts</a>
-      </div>
-    </div>
-  )
-}
+// Previous implementation using hover card
+// const IndividualBanner: React.FC<ContentProps> = ({ profile }) => (
+//  <HoverCard.Root openDelay={300}>
+//    <HoverCard.Trigger>
+//      <ContentTrigger profile={profile} />
+//    </HoverCard.Trigger>
+//    <HoverCard.Portal>
+//      <HoverCard.Content sideOffset={10} side='top' className='z-40'>
+//        <Card profile={profile} />
+//      </HoverCard.Content>
+//    </HoverCard.Portal>
+//  </HoverCard.Root>
+// )
+
+// const ContentTrigger: React.FC<ContentProps> = ({ profile }) => {
+//  const { name, website } = profile
+//  return (
+//    <a
+//      className='block uppercase hover:underline font-medium'
+//      href={website}
+//      target='_blank'
+//      rel='noreferrer'
+//    >
+//      {name}
+//    </a>
+//  )
+// }
+
+// const Card: React.FC<ContentProps> = ({ profile }) => {
+//  const { name, website, instagram, report, donation } = profile
+//  return (
+//    <div className='card w-96 bg-secondary shadow-xl z-50 overflow-clip border-base-200'>
+//      <div className='p-8 pt-10'>
+//        <h2 className='card-title my-2 uppercase'>{name}</h2>
+//        <div className='flex gap-4'>
+//          <a className='badge badge-outline opacity-60' href={website} target='_blank' rel='noreferrer'>website</a>
+//          <a className='badge badge-outline opacity-60' href={instagram} target='_blank' rel='noreferrer'>instagram</a>
+//          {donation != null && <a className='badge opacity-60 px-4' href={donation} target='_blank' rel='noreferrer'>Donate</a>}
+//        </div>
+//      </div>
+//      <div className='card-actions bg-base-100 flex justify-end p-4'>
+//        <a className='btn btn-primary btn-sm opacity-80' href={report} target='_blank' rel='noreferrer'>Report bad bolts</a>
+//      </div>
+//    </div>
+//  )
+// }
