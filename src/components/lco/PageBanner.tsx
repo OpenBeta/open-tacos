f63290af15031d4a56ab701449c6e95349c8a3fa
Yun Ji <this.jiyun@gmail.com>
2023-05-16 17:25:07 +0200

    clean commented code(previous implementation

--- src/components/lco/PageBanner.tsx
+++ src/components/lco/PageBanner.tsx
@@ -111,34 +111,6 @@ interface ContentProps {
   profile: LCOProfileType
 }
 
-// Previous implementation using hover card
-// const IndividualBanner: React.FC<ContentProps> = ({ profile }) => (
-//  <HoverCard.Root openDelay={300}>
-//    <HoverCard.Trigger>
-//      <ContentTrigger profile={profile} />
-//    </HoverCard.Trigger>
-//    <HoverCard.Portal>
-//      <HoverCard.Content sideOffset={10} side='top' className='z-40'>
-//        <Card profile={profile} />
-//      </HoverCard.Content>
-//    </HoverCard.Portal>
-//  </HoverCard.Root>
-// )
-
-// const ContentTrigger: React.FC<ContentProps> = ({ profile }) => {
-//  const { name, website } = profile
-//  return (
-//    <a
-//      className='block uppercase hover:underline font-medium'
-//      href={website}
-//      target='_blank'
-//      rel='noreferrer'
-//    >
-//      {name}
-//    </a>
-//  )
-// }
-
 const LcoCard: React.FC<ContentProps> = ({ profile }) => {
   const { name, website, instagram, report, donation } = profile
   return (
@@ -232,22 +204,3 @@ const LcoCard: React.FC<ContentProps> = ({ profile }) => {
 
   )
 }
-
-// const Card: React.FC<ContentProps> = ({ profile }) => {
-//  const { name, website, instagram, report, donation } = profile
-//  return (
-//    <div className='card w-96 bg-secondary shadow-xl z-50 overflow-clip border-base-200'>
-//      <div className='p-8 pt-10'>
-//        <h2 className='card-title my-2 uppercase'>{name}</h2>
-//        <div className='flex gap-4'>
-//          <a className='badge badge-outline opacity-60' href={website} target='_blank' rel='noreferrer'>website</a>
-//          <a className='badge badge-outline opacity-60' href={instagram} target='_blank' rel='noreferrer'>instagram</a>
-//          {donation != null && <a className='badge opacity-60 px-4' href={donation} target='_blank' rel='noreferrer'>Donate</a>}
-//        </div>
-//      </div>
-//      <div className='card-actions bg-base-100 flex justify-end p-4'>
-//        <a className='btn btn-primary btn-sm opacity-80' href={report} target='_blank' rel='noreferrer'>Report bad bolts</a>
-//      </div>
-//    </div>
-//  )
-// }
