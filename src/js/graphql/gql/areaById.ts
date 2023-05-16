9fe5c70bf7f1c176c0c7bdafca561cc2880e35bc
Yun Ji <this.jiyun@gmail.com>
2023-05-16 16:44:30 +0200

    add organizations field to area query

--- src/js/graphql/gql/areaById.ts
+++ src/js/graphql/gql/areaById.ts
@@ -47,6 +47,20 @@ export const QUERY_AREA_BY_ID = gql`
       }
       pathTokens  
       ancestors
+      organizations{
+        orgId
+        associatedAreaIds
+        displayName
+        content{
+          description
+          donationLink
+          email
+          facebookLink
+          hardwareReportLink
+          instagramLink
+          website
+        }
+      }
       climbs {
         id
         name
