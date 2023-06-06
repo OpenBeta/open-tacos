d1a61f2bcee9520b643ddea5df5904cb12a4fc82
viet nguyen <vietnguyen@noreply>
2023-06-05 22:14:50 -0700

    chore: fix typescript errors

--- src/js/types/next-auth.d.ts
+++ src/js/types/next-auth.d.ts
@@ -2,17 +2,22 @@ import { DefaultUser } from 'next-auth'
 import { IUserMetadata } from './User'
 declare module 'next-auth' {
   /**
-   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context.
+   * See also https://next-auth.js.org/getting-started/typescript
    */
   interface Session {
     user: DefaultUser & {
       metadata: IUserMetadata
     }
+    accessToken: string
+    id: string
   }
 }
 
 declare module 'next-auth/jwt' {
   interface JWT {
     userMetadata: IUserMetadata
+    id: string
+    accessToken: string
   }
 }
