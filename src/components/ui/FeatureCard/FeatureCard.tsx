82bdda11d9d63449f4802b3cb8eb4ef6eebbdda9
Ali Haider <ha4219466@gmail.com>
2023-06-11 10:06:47 +0500

    changes

--- src/components/ui/FeatureCard/FeatureCard.tsx
+++ src/components/ui/FeatureCard/FeatureCard.tsx
@@ -13,7 +13,8 @@ function FeatureCard ({ area }: { area: AreaType }): JSX.Element {
   if (media != null && media.length > 0) {
     imageUrl = `${CLIENT_CONFIG.CDN_BASE_URL}/${shuffle(media)[0].mediaUrl}?format=webp&h=300&q=90`
   }
-
+// console.log("imageUrl", imageUrl)
+// console.log("media", media)
   const image = {
     url: imageUrl,
     license: 'All Rights Reserved',
