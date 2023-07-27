4e12816c4afce9ee006091333835a0797cc0b9b8
viet nguyen <vietnguyen@noreply>
2023-07-27 10:31:12 -0700

    fix: add donation & github links to menu

--- src/components/MobileAppBar.tsx
+++ src/components/MobileAppBar.tsx
@@ -11,6 +11,7 @@ import { signIn, signOut, useSession } from 'next-auth/react'
 import ProfileNavButton from './ProfileNavButton'
 import NewPost from './NewPost'
 import { XSearchMobile } from './search/XSearch'
+import { MouseEventHandler } from 'react'
 
 interface HeaderProps {
   includeFilters: boolean
@@ -44,7 +45,7 @@ const AuthenticatedNav = (): JSX.Element => (
 
 const LoginButton = (): JSX.Element => (
   <Button
-    label={<span><span className='inline md:hidden'>Login</span><span className='hidden md:inline text-sm'>Become A Contributor</span></span>}
+    label='Login'
     onClick={async () => await signIn('auth0', { callbackUrl: '/api/user/me' })}
     variant={ButtonVariant.SOLID_PRIMARY}
   />)
@@ -74,28 +75,40 @@ const More = (): JSX.Element => {
         <Button label={<Bars3Icon className='text-white w-8 h-8' />} />
       </Popover.Button>
 
-      <Popover.Panel className='absolute z-20 right-0 mt-2 p-6 bg-white rounded-md'>
+      <Popover.Panel className='absolute z-20 right-0 mt-2 p-6 bg-white rounded-md w-full max-w-md'>
         <div className='grid'>
           {status === 'authenticated'
-            ? <Button
-                size='lg'
-                onClick={async () => {
-                  sessionStorage.setItem('editMode', 'false')
-                  await signOut({ callbackUrl: `${window.origin}/api/auth/logout` })
-                }}
-                label='Logout'
-              />
-            : <Button size='lg' onClick={async () => await signIn()} label='Login' />}
-          <Button size='lg' href='/about' label='About' />
-          <Button size='lg' href='https://openbeta.io/blog' label='Blog' />
-          <Button
-            size='lg'
-            href='https://discord.gg/ptpnWWNkJx'
-            label='Discord'
-            variant={ButtonVariant.SOLID_SECONDARY}
-          />
+            ? (
+              <>
+                <a className='btn btn-ghost no-animation btn-block' href='/api/user/me?preview=1'><span className='pl-10'>Dashboard</span> <sup className='ml-2 badge badge-sm badge-primary'>Beta</sup></a>
+                <a className='btn btn-ghost no-animation btn-block' href='/account/editProfile'>Account settings</a>
+                <a className='btn btn-ghost no-animation btn-block' href='/' onClick={logoutHandler}>Logout</a>
+              </>)
+            : <a className='btn btn-accent no-animation btn-block' href='/' onClick={signInHandler}>Login</a>}
+          <hr />
+          <a className='btn btn-ghost no-animation btn-block text-accent' href='https://opencollective.com/openbeta/contribute/t-shirt-31745'>Get your OpenBeta T-shirts</a>
+          <hr />
+          <a className='btn btn-ghost no-animation btn-block' href='/about'>About</a>
+          <a className='btn btn-ghost no-animation btn-block' href='https://docs.openbeta.io'>Documentation</a>
+          <a className='btn btn-ghost no-animation btn-block' href='https://openbeta.io/blog'>Blog</a>
+          <hr />
+          <a className='btn btn-ghost no-animation btn-block' href='https://github.com/OpenBeta/open-tacos'>GitHub</a>
+          <a className='btn btn-outline no-animation btn-block' href='https://discord.gg/ptpnWWNkJx'>Discord community</a>
         </div>
       </Popover.Panel>
     </Popover>
   )
+}
+
+const logoutHandler: MouseEventHandler = async (event) => {
+  event.stopPropagation()
+  event.preventDefault()
+  sessionStorage.setItem('editMode', 'false')
+  await signOut({ callbackUrl: `${window.origin}/api/auth/logout` })
+}
+
+const signInHandler: MouseEventHandler = async (event) => {
+  event.stopPropagation()
+  event.preventDefault()
+  await signIn('auth0')
 }
