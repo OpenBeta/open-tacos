c3385689d492de7ee52d0feb1badc2f291448db0
clintonlunn <cpl09c@gmail.com>
2023-09-18 20:57:11 -0600

    uncommenting tests

--- src/components/media/__tests__/PhotoMontage.tsx
+++ src/components/media/__tests__/PhotoMontage.tsx
@@ -2,30 +2,21 @@ import { render, screen } from '@testing-library/react'
 import PhotoMontage from '../PhotoMontage'
 import { mediaList } from './data'
 
-// test('PhotoMontage can render 1 photo', async () => {
-//   render(<PhotoMontage photoList={mediaList.slice(0, 1)} isHero />)
-//   const elements: HTMLImageElement[] = await screen.findAllByRole('img')
-//   expect(elements.length).toBe(1)
-//   expect(elements[0].src).toContain(mediaList[0].mediaUrl)
-// })
-
-// test('PhotoMontage always renders 2 photos when provided with a list of 2 to 4', async () => {
-//   render(<PhotoMontage photoList={mediaList.slice(0, 3)} isHero />)
-//   const elements: HTMLImageElement[] = await screen.findAllByRole('img')
-//   expect(elements.length).toBe(2) // should be 2
-// })
-
-// test('PhotoMontage always renders 5 photos when provided with a list > 5', async () => {
-//   render(<PhotoMontage photoList={mediaList} isHero />)
-//   const elements: HTMLImageElement[] = await screen.findAllByRole('img')
-//   expect(elements.length).toBe(5) // should be 5
-// })
-
-describe('basic true test', () => {
-  test('true is true', () => {
-    expect(true).toBe(true)
-  })
+test('PhotoMontage can render 1 photo', async () => {
+  render(<PhotoMontage photoList={mediaList.slice(0, 1)} isHero />)
+  const elements: HTMLImageElement[] = await screen.findAllByRole('img')
+  expect(elements.length).toBe(1)
+  expect(elements[0].src).toContain(mediaList[0].mediaUrl)
 })
 
-// commenting out to get the commit to pass
-// will need to fix this later
+test('PhotoMontage always renders 2 photos when provided with a list of 2 to 4', async () => {
+  render(<PhotoMontage photoList={mediaList.slice(0, 3)} isHero />)
+  const elements: HTMLImageElement[] = await screen.findAllByRole('img')
+  expect(elements.length).toBe(2) // should be 2
+})
+
+test('PhotoMontage always renders 5 photos when provided with a list > 5', async () => {
+  render(<PhotoMontage photoList={mediaList} isHero />)
+  const elements: HTMLImageElement[] = await screen.findAllByRole('img')
+  expect(elements.length).toBe(5) // should be 5
+})
