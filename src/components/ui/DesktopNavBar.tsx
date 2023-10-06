336ecf3ad57e3bad81ad72c1d02434164b9b8aa3
Syed Irfan Faraz <irfanfaraaz03@gmail.com>
2023-10-07 01:13:21 +0530

    fix: refine width styling in search component

--- src/components/ui/DesktopNavBar.tsx
+++ src/components/ui/DesktopNavBar.tsx
@@ -21,7 +21,7 @@ export default function DesktopNavBar ({ branding, search, navList }: DesktopApp
     >
       <div className='w-fit'>{branding}</div>
 
-      <div className='w-2/5 2xl:w-full'>
+      <div className='w-2/5'>
         {search}
       </div>
 
