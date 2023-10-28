82d481898c6cf61b7071c4a869bdc0bb5e1612d9
viet nguyen <vietnguyen@noreply>
2023-10-28 12:18:44 -0700

    fix: improve CTA
    feat: add loading skeleton to recent edits

--- src/app/components/RecentEdits.tsx
+++ src/app/components/RecentEdits.tsx
@@ -1,9 +1,17 @@
+import Link from 'next/link'
 import { ArrowRightIcon } from '@heroicons/react/24/outline'
 
 import { getChangeHistoryServerSide } from '@/js/graphql/contribAPI'
 import { ChangesetRow } from '@/components/edit/RecentChangeHistory'
-import Link from 'next/link'
 
+/**
+ * Cache time in seconds
+ */
+export const revalidate = 300
+
+/**
+ * Show most recent edits
+ */
 export const RecentEdits: React.FC = async () => {
   const history = await getChangeHistoryServerSide()
   return (
@@ -15,10 +23,31 @@ export const RecentEdits: React.FC = async () => {
       <hr className='mb-6 border-2 border-base-content' />
       <div className='mt-4 flex justify-center flex-row flex-wrap gap-y-10 gap-x-4'>
         {history.splice(0, 10).map(changetset =>
-          <ChangesetRow key={changetset.id} changeset={changetset} />)}
+          <ChangesetRow key={changetset.id} changeset={changetset} />
+        )}
+
       </div>
       <div className='flex justify-center py-10'>
         <Link href='/edit' className='btn btn-sm btn-outline'>See more <ArrowRightIcon className='w-4 h-4' /></Link>
+      </div>
+    </section>
+  )
+}
+
+/**
+ * Loading skelton.
+ * Todo: somehow reuse the structure and css from the real component
+ */
+export const RecentEditsSkeleton: React.FC = () => {
+  return (
+    <section className='px-4 w-full'>
+      <div className='flex items-center justify-between'>
+        <h2>Recent edits</h2>
+        <Link href='/edit' className='text-sm hover:underline'>See more</Link>
+      </div>
+      <hr className='mb-6 border-2 border-base-content' />
+      <div className='mt-4 flex justify-center flex-row flex-wrap gap-y-10 gap-x-4'>
+        {[1, 2, 3, 4, 5].map(item => <div key={item} className='animate-pulse w-full bg-base-200/30 h-36 rounded-box' />)}
       </div>
     </section>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
+import { Suspense } from 'react'
 import { LandingCTA } from './components/LandingCTA'
 import { LandingHero } from './components/LandingHero'
-import { RecentEdits } from './components/RecentEdits'
+import { RecentEdits, RecentEditsSkeleton } from './components/RecentEdits'
 import { RecentTags } from './components/RecentTags'
 import { USAToC } from './components/USAToC'
 
@@ -8,13 +9,15 @@ export default async function Home (): Promise<any> {
   return (
     <div className='mt-8 w-full flex flex-col gap-y-24'>
       <div className='lg:pl-4 lg:grid lg:grid-cols-3'>
-        <div className='col-span-2 pb-10 bg-gradient-to-r from-cyan-500/40 to-blue-500/60 rounded-box'>
+        <div className='col-span-2 pb-10 rounded-box'>
           <LandingHero />
           <LandingCTA />
           <p className='mt-12 px-10 text-center text-sm'>Our website is undergoing a facelift. Visit the <a href='/classic' className='underline'>old home</a>. <br /><span className='text-base-content/60'>Questions or comments? <a href='mailto:hello@openbeta.io'>hello@openbeta.io</a></span></p>
         </div>
         <div className='mt-8 lg:mt-0 lg:overflow-y-auto lg:h-[800px] w-full'>
-          <RecentEdits />
+          <Suspense fallback={<RecentEditsSkeleton />}>
+            <RecentEdits />
+          </Suspense>
         </div>
       </div>
       <RecentTags />
