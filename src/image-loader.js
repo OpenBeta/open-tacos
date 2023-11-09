f7c1d3bd3ec4422cc8d8abe2a28058ae81fdfeaf
Iacami Gevaerd <enapupe@gmail.com>
2023-11-09 09:18:27 -0300

    fix: remove usage of MobileLoader and remove hard webp format

--- src/image-loader.js
+++ src/image-loader.js
@@ -3,8 +3,7 @@ const imageLoader = ({ src, width, quality }) => {
   if (src.includes(BASE_URL)) {
     return src
   }
-  // The case below does not seem to be handled by this image-loader, it's here just in case:
-  return `${process.env.NEXT_PUBLIC_CDN_URL}/${src}?w=${width}&q=${quality || 75}`
+  return `${process.env.NEXT_PUBLIC_CDN_URL}${src}?w=${width}&q=${quality || 75}`
 }
 
 export default imageLoader
