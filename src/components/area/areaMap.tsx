b770b94b31d47b63c906ad316aa2298fa8978559
Ali Muhammad <81983704+Ali-muhammed7040@users.noreply.github.com>
2023-11-13 20:18:04 +0500

    improve area map (#1014)
    fix: improve area map  #1009

--- src/components/area/areaMap.tsx
+++ src/components/area/areaMap.tsx
@@ -1,5 +1,11 @@
 import * as React from 'react'
-import { Map, Marker, ScaleControl, LngLatBoundsLike, MapboxMap } from 'react-map-gl'
+import {
+  Map,
+  Marker,
+  ScaleControl,
+  LngLatBoundsLike,
+  MapboxMap
+} from 'react-map-gl'
 import { AreaType } from '../../js/types'
 import { MAP_STYLES } from '../maps/BaseMap'
 import { Padding } from '@math.gl/web-mercator/dist/fit-bounds'
@@ -31,10 +37,14 @@ function getBounds (area: AreaType): [[number, number], [number, number]] {
   if (area.children.length > 0) {
     area.children.forEach((area) => {
       const { lat, lng } = area.metadata
-      if (lat > maxLat) { maxLat = lat } else if (lng > maxLon) {
+      if (lat > maxLat) {
+        maxLat = lat
+      } else if (lng > maxLon) {
         maxLon = lng
       }
-      if (lat < minLat) { minLat = lat } else if (lng < minLon) {
+      if (lat < minLat) {
+        minLat = lat
+      } else if (lng < minLon) {
         minLon = lng
       }
     })
@@ -42,7 +52,8 @@ function getBounds (area: AreaType): [[number, number], [number, number]] {
 
   return [
     [minLon, minLat], // SouthWest corner
-    [maxLon, maxLat]] // northeastern corner of the bounds
+    [maxLon, maxLat]
+  ] // northeastern corner of the bounds
 }
 
 function computeVS (area: AreaType): LngLatBoundsLike {
@@ -51,12 +62,15 @@ function computeVS (area: AreaType): LngLatBoundsLike {
 
 export default function AreaMap (props: AreaMapProps): JSX.Element {
   const mapRef = React.useRef(null)
-  const padding: Padding = { top: 45, left: 45, bottom: 45, right: 45 }
+  let padding: Padding = { top: 45, left: 45, bottom: 45, right: 45 }
+  if (props.subAreas.length === 0) {
+    padding = { top: 100, left: 100, bottom: 100, right: 100 }
+  }
 
   React.useEffect(() => {
     // re-compute bounds whenever the area changes
     if (mapRef.current !== null) {
-      const map: MapboxMap = (mapRef.current as any)
+      const map: MapboxMap = mapRef.current as any
       const bounds = computeVS(props.area)
       map.fitBounds(bounds, { padding })
     }
@@ -73,10 +87,10 @@ export default function AreaMap (props: AreaMapProps): JSX.Element {
         }}
         reuseMaps
         mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_API_KEY}
-        mapStyle={MAP_STYLES.dark}
+        mapStyle={MAP_STYLES.light}
         cooperativeGestures
       >
-        {props.subAreas.map(subArea => (
+        {props.subAreas.map((subArea) => (
           <Marker
             style={{ zIndex: subArea.id === props.focused ? 100 : 0 }}
             key={subArea.id}
@@ -84,15 +98,30 @@ export default function AreaMap (props: AreaMapProps): JSX.Element {
             latitude={subArea.metadata.lat}
             anchor='bottom'
           >
-            <div className={`rounded shadow transition text-xs px-1 font-bold 
-            ${subArea.id === props.focused ? 'bg-green-500 text-white scale-125' : ' bg-white'}
-            ${subArea.id === props.selected ? 'bg-violet-500 text-white scale-125' : ' bg-white'}`}
+            <div
+              className={`rounded shadow transition text-xs px-1 font-bold 
+            ${
+              subArea.id === props.focused
+                ? 'bg-green-500 text-white scale-125'
+                : 'bg-white'
+            }
+            ${
+              subArea.id === props.selected
+                ? 'bg-violet-500 text-white scale-125'
+                : 'bg-white'
+            }`}
             >
               {subArea.areaName}
             </div>
-          </Marker>))}
+          </Marker>
+        ))}
 
-        <Marker style={{ zIndex: 1 }} longitude={props.area.metadata.lng} latitude={props.area.metadata.lat} anchor='bottom'>
+        <Marker
+          style={{ zIndex: 1 }}
+          longitude={props.area.metadata.lng}
+          latitude={props.area.metadata.lat}
+          anchor='bottom'
+        >
           <div className='rounded shadow transition text-md px-1 font-bold bg-violet-500 text-white scale-125 z-index-1'>
             {props.area.areaName}
           </div>
