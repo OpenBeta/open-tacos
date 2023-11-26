6bd8885cde20f42955c3b11bbda32729161243e3
viet nguyen <vietnguyen@noreply>
2023-11-26 10:26:19 -0800

    add missing files 2

---
+++ src/app/components/AreaPageActions.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+import { PencilSimple, ArrowElbowLeftDown } from '@phosphor-icons/react/dist/ssr'
+import { ShareAreaLinkButton } from '@/app/components/ShareAreaLinkButton'
+
+/**
+ * Main action bar for area page
+ */
+export const AreaPageActions: React.FC<{ uuid: string, areaName: string } > = ({ uuid, areaName }) => (
+  <ul className='mt-6 flex items-center justify-between'>
+    <Link href={`/editArea/${uuid}`} className='btn btn-primary btn-sm'>
+      <PencilSimple size={20} weight='duotone' /> Edit
+    </Link>
+    <Link href='#map' className='btn btn-sm'>
+      <ArrowElbowLeftDown size={20} /> Map
+    </Link>
+    <ShareAreaLinkButton uuid={uuid} areaName={areaName} />
+  </ul>
+)
