977f11f27a4402bf9a85c1a0a0820c97e36c6ffa
Viet Nguyen <3805254+vnugent@users.noreply.github.com>
2024-01-20 20:20:39 -0700

    feat: show crag info on hover (#1081)

--- src/components/maps/AreaActiveMarker.tsx
+++ src/components/maps/AreaActiveMarker.tsx
@@ -1,12 +1,44 @@
-import { Marker } from 'react-map-gl'
-import { Point } from '@turf/helpers'
+import { Marker, Source, Layer, LineLayer } from 'react-map-gl'
+import { Point, Polygon } from '@turf/helpers'
 import { MapPin } from '@phosphor-icons/react/dist/ssr'
 
-export const AreaActiveMarker: React.FC<{ point: Point }> = ({ point }) => {
-  const { coordinates } = point
+/**
+ * Highlight selected feature on the map
+ */
+export const SelectedFeature: React.FC<{ geometry: Point | Polygon }> = ({ geometry }) => {
+  switch (geometry.type) {
+    case 'Point':
+      return <SelectedPoint geometry={geometry} />
+    case 'Polygon':
+      return <SelectedPolygon geometry={geometry} />
+    default: return null
+  }
+}
+
+const SelectedPoint: React.FC<{ geometry: Point }> = ({ geometry }) => {
+  const { coordinates } = geometry
   return (
-    <Marker longitude={coordinates[0]} latitude={coordinates[1]} style={{ zIndex: 2000 }}>
+    <Marker longitude={coordinates[0]} latitude={coordinates[1]}>
       <MapPin size={36} weight='fill' className='text-accent' />
     </Marker>
   )
+}
+
+export const SelectedPolygon: React.FC<{ geometry: Polygon }> = ({ geometry }) => {
+  return (
+    <Source id='selected-polygon' type='geojson' data={geometry}>
+      <Layer {...selectedBoundary} />
+    </Source>
+  )
+}
+
+const selectedBoundary: LineLayer = {
+  id: 'polygon2',
+  type: 'line',
+  paint: {
+    'line-opacity': ['step', ['zoom'], 0.85, 10, 0.5],
+    'line-width': ['step', ['zoom'], 2, 10, 10],
+    'line-color': '#004F6E', // See 'area-cue' in tailwind.config.js
+    'line-blur': 4
+  }
 }
--- src/components/maps/AreaInfoHover.tsx
+++ src/components/maps/AreaInfoHover.tsx
@@ -1,36 +1,37 @@
 import * as Popover from '@radix-ui/react-popover'
-
-import { MapAreaFeatureProperties } from './AreaMap'
+import { HoverInfo, MapAreaFeatureProperties } from './AreaMap'
 import { getAreaPageFriendlyUrl } from '@/js/utils'
 import { Card } from '../core/Card'
 import { EntityIcon } from '@/app/editArea/[slug]/general/components/AreaItem'
-import { ArrowRight } from '@phosphor-icons/react/dist/ssr'
-
+import { SelectedPolygon } from './AreaActiveMarker'
 /**
  * Area info panel
  */
-export const AreaInfoDrawer: React.FC<{ data: MapAreaFeatureProperties | null, onClose?: () => void }> = ({ data, onClose }) => {
+export const AreaInfoHover: React.FC<HoverInfo> = ({ data, geometry, mapInstance }) => {
   const parent = data?.parent == null ? null : JSON.parse(data.parent)
-  const parentName = parent?.name ?? ''
+  const parentName = parent?.name ?? 'Unknown'
   const parentId = parent?.id ?? null
+
+  let screenXY
+  if (geometry.type === 'Point') {
+    screenXY = mapInstance.project(geometry.coordinates)
+  } else {
+    return <SelectedPolygon geometry={geometry} />
+  }
   return (
-    <Popover.Root open={data != null}>
-      <Popover.Anchor className='absolute top-3 left-3 z-50' />
-      <Popover.Content align='start'>
+    <Popover.Root defaultOpen>
+      <Popover.Anchor style={{ position: 'absolute', left: screenXY.x, top: screenXY.y }} />
+      <Popover.Content align='center' side='top' alignOffset={12}>
         {data != null && <Content {...data} parentName={parentName} parentId={parentId} />}
+        <Popover.Arrow />
       </Popover.Content>
     </Popover.Root>
   )
 }
 
-export const Content: React.FC<MapAreaFeatureProperties & { parentName: string, parentId: string | null }> = ({ id, name, parentName, parentId, content }) => {
+export const Content: React.FC<MapAreaFeatureProperties & { parentName: string, parentId: string | null }> = ({ id, name, parentName, parentId }) => {
   const url = parentId == null
-    ? (
-      <div className='inline-flex items-center gap-1.5'>
-        <EntityIcon type='area' size={16} />
-        <span className='text-secondary font-medium'>{parentName}</span>
-      </div>
-      )
+    ? parentName
     : (
       <a
         href={getAreaPageFriendlyUrl(parentId, name)}
@@ -39,8 +40,6 @@ export const Content: React.FC<MapAreaFeatureProperties & { parentName: string, 
         <EntityIcon type='area' size={16} /><span className='text-secondary font-medium hover:underline '>{parentName}</span>
       </a>
       )
-
-  const friendlyUrl = getAreaPageFriendlyUrl(id, name)
   return (
     <Card>
       <div className='flex flex-col gap-y-1 text-xs'>
@@ -48,11 +47,6 @@ export const Content: React.FC<MapAreaFeatureProperties & { parentName: string, 
         <div className='ml-2'>
           <span className='text-secondary'>&#8735;</span><a href={getAreaPageFriendlyUrl(id, name)} className='text-sm font-medium hover:underline'>{name}</a>
         </div>
-      </div>
-      <hr className='mt-6' />
-      <div className='flex items-center justify-end gap-2'>
-        <a className='btn btn-link btn-sm no-underline' href={`/editArea/${id}`}>Edit</a>
-        <a className='btn btn-primary btn-sm' href={friendlyUrl}>Visit area <ArrowRight /></a>
       </div>
     </Card>
   )
