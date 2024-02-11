36dc570d33eefb1c418d3d3d9266c0799085e0be
l4u532 <88317742+l4u532@users.noreply.github.com>
2024-02-11 11:41:00 +0100

    add empty bulk-upload page

---
+++ src/pages/bulk-upload.tsx
@@ -0,0 +1,18 @@
+import Layout from '../components/layout'
+
+const BulkUpload = (): JSX.Element => {
+  return (
+    <Layout contentContainerClass='content-default' showFilterBar={false}>
+      <div className='max-w-screen-md mx-auto py-8 px-4 md:px-8 md:py-12'>
+        <div className='text-center'>
+          <h1>Bulk Upload Page</h1>
+          <p className='text-xl text-slate-700 my-12'>
+            This page is under construction. Please check back later for updates.
+          </p>
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
+export default BulkUpload
