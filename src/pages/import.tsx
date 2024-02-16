800a892b37147900d68b811647cf14eb98a5b9ba
l4u532 <88317742+l4u532@users.noreply.github.com>
2024-02-16 13:26:08 +0100

    polish styling

--- src/pages/import.tsx
+++ src/pages/import.tsx
@@ -9,7 +9,7 @@ const addFormats = require("ajv-formats");
 const betterAjvErrors = require("better-ajv-errors").default
 
 
-function FileUploadClientComponent() {
+function FileUploadAndValidationClientComponent() {
   const fileInputRef = useRef<HTMLInputElement>(null)
   const [fileName, setFileName] = useState<string | null>(null);
   const [validationErrors, setValidationErrors] = useState([])
@@ -84,46 +84,45 @@ function FileUploadClientComponent() {
   };
 
   return (
-    <><div className="alert mt-2">
+    <><div className="alert mt-2 flex flex-col gap-2 items-start">
+      <div className="flex flex-row ">
+        <h2>File Upload</h2>
+      </div>
+      
 
-      <File size={24} />
-                <h2>Validation Result</h2>
-
+      <div className="flex flex-col items-center">
       <input
         accept="application/json"
         style={{ display: "none" }}
         id="contained-button-file"
         type="file"
         onChange={(event) => {
-          handleFileSelect(event);
+          handleFileSelect(event)
           if (event.target.files && event.target.files[0]) {
-            setFileName(event.target.files[0].name); // Assuming setFileName is a state setter function defined to hold the file name
+            setFileName(event.target.files[0].name)
           }
         }}
         ref={fileInputRef} />
 
-      <label htmlFor="contained-button-file">
-        <button
-          className='btn btn-solid btn-accent shadow-md'
-          onClick={handleButtonClick}
-        >
-          Select JSON File to Upload
-        </button>
-      </label>
-      <p>{fileName ? `Selected file: ${fileName}` : "No file selected."}</p> {/* Display the selected file name or indicate no file selected */}
 
-      
+        <label htmlFor="contained-button-file">
+          <button
+            className='btn btn-lg btn-neutral'
+            onClick={handleButtonClick}
+          >
+            📁 Select JSON File to Upload
+          </button>
+        </label>
+        </div>  
+        <p>{fileName ? `Selected file: ${fileName}` : "No file selected."}</p>
+
+
     </div>
-      <div className="alert mt-2">
-        <Detective size={24} />
-
-        <div >
-
-          <h2>Validation Result</h2>
-          {validationErrors.length === 0 ? (
-            <p>No errors found.</p>
-          ) : (
-            validationErrors.map((error, index) => (
+      {validationErrors.length > 0 && (
+        <div className="alert mt-2">
+          <div>
+            <h2>Validation Errors</h2>
+            {validationErrors.map((error, index) => (
               <div key={index}>
                 <div>
                   <h6>{error['error']}</h6>
@@ -132,14 +131,14 @@ function FileUploadClientComponent() {
                   <pre style={{ fontSize: '10px', whiteSpace: 'pre-wrap', wordWrap: 'break-word' }}>{JSON.stringify(error, null, 2)}</pre>
                 </div>
               </div>
-            ))
-          )}
+            ))}
+          </div>
         </div>
-      </div>
+      )}
       <br></br>
       <div className="flex justify-center">
         <div className='tooltip' data-tip="Your JSON file must be successfully validated first">
-          <button disabled={!isValidationSuccessful} className='btn btn-primary shadow-lg hover:scale-110 duration-300 bg-accent text-accent-content btn-lg'>
+          <button disabled={!isValidationSuccessful} className='btn btn-primary btn-wide shadow-lg hover:scale-110 duration-300 bg-accent text-accent-content btn-lg'>
             Upload to Database
           </button>
         </div>
@@ -154,24 +153,23 @@ const BulkImport = (): JSX.Element => {
     <Layout contentContainerClass='content-default' showFilterBar={false}>
       <div className="max-w-4xl mx-auto p-4">
         <h1 className="text-3xl font-bold mb-4">
-          Bulk Data Import V2
+          Bulk Data Import
         </h1>
         <div role='alert' className='alert alert-info flex flex-row items-start gap-2'>
           <Lightbulb size={24} />
           <div className='text-sm'>
             <ul className="list-disc pl-5">
               <li>Allows for adding or changing large amounts of data at once.</li>
-              <li>Utilizes <a href="./schema/schema.json" target="_blank" rel="noopener noreferrer" className="link">JSON schema</a> to ensure correct data structure.</li>
-              <li>Example Uploads and detailed instructions are available in the <a href="./schema/README.md" target="_blank" rel="noopener noreferrer" className="link">README</a>.</li>
+              <li>Utilizes this <a href="./schema/schema.json" target="_blank" rel="noopener noreferrer" className="link">JSON schema</a> to ensure correct data structure.</li>
+              <li>Example upload files and detailed instructions are available in this <a href="./schema/README.md" target="_blank" rel="noopener noreferrer" className="link">README</a>.</li>
+              <br></br>
               <li>Note: OpenBeta’s route database is licensed as CC-BY-SA 4.0.</li>
-              <li>Special permissions required (ask on <a href="https://discord.gg/a6vuuTQxS8" className="link">Discord</a>).</li>
+              <li>Need help? Find us on <a href="https://discord.gg/a6vuuTQxS8" className="link">Discord</a>.</li>
             </ul>
           </div>
         </div>
         <div className="my-8">
-          <React.Suspense fallback={<div>Loading...</div>}>
-            <FileUploadClientComponent />
-          </React.Suspense>
+          <FileUploadAndValidationClientComponent />
         </div>
 
       </div>
