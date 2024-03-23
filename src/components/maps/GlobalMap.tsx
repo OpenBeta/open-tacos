68fdc74b1398cd1a2616a6ccb9852687a43509f1
clintonlunn <cpl09c@gmail.com>
2024-03-23 16:20:13 -0600

    feat: track map center and zoom in url, initial load map url params (#1113)

--- src/components/maps/GlobalMap.tsx
+++ src/components/maps/GlobalMap.tsx
@@ -12,6 +12,7 @@ import { SelectedFeature } from './AreaActiveMarker'
 import { OBCustomLayers } from './OBCustomLayers'
 import { AreaType, ClimbType, MediaWithTags } from '@/js/types'
 import { TileProps, transformTileProps } from './utils'
+import Spinner from '../ui/Spinner'
 
 export type SimpleClimbType = Pick<ClimbType, 'id' | 'name' | 'type'>
 
@@ -30,6 +31,7 @@ export interface HoverInfo {
 interface GlobalMapProps {
   showFullscreenControl?: boolean
   initialCenter?: [number, number]
+  initialZoom?: number
   initialViewState?: {
     bounds: maplibregl.LngLatBoundsLike
     fitBoundsOptions: maplibregl.FitBoundsOptions
@@ -37,40 +39,64 @@ interface GlobalMapProps {
   children?: React.ReactNode
 }
 
+interface GlobalMapSkeletonProps extends React.HTMLAttributes<HTMLDivElement> {
+  width?: string
+  height?: string
+}
+
+export const GlobalMapSkeleton: React.FC<GlobalMapSkeletonProps> = ({
+  width = '100%',
+  height = '100%',
+  ...props
+}) => {
+  return (
+    <div
+      className='relative animate-pulse rounded-md bg-gray-300'
+      style={{ width, height }}
+      {...props}
+    >
+      <div className='flex justify-center items-center h-full'>
+        <Spinner />
+      </div>
+    </div>
+  )
+}
+
 /**
  * Global map
  */
 export const GlobalMap: React.FC<GlobalMapProps> = ({
-  showFullscreenControl = true, initialCenter, initialViewState, children
+  showFullscreenControl = true, initialCenter, initialZoom, initialViewState, children
 }) => {
   const [clickInfo, setClickInfo] = useState<MapAreaFeatureProperties | null>(null)
   const [hoverInfo, setHoverInfo] = useState<HoverInfo | null>(null)
   const [selected, setSelected] = useState<Point | Polygon | null>(null)
   const [mapInstance, setMapInstance] = useState<MapInstance | null>(null)
   const [cursor, setCursor] = useState<string>('default')
+  const [mapLoaded, setMapLoaded] = useState(false)
 
   const onLoad = useCallback((e: MapLibreEvent) => {
     setMapInstance(e.target)
-    if (initialCenter != null) {
-      e.target.jumpTo({ center: initialCenter, zoom: 6 })
+    if (initialCenter != null && initialZoom != null) {
+      e.target.jumpTo({ center: initialCenter, zoom: initialZoom })
     } else if (initialViewState != null) {
       e.target.fitBounds(initialViewState.bounds, initialViewState.fitBoundsOptions)
     }
+    setMapLoaded(true)
   }, [initialCenter])
 
   /**
    * Handle click event on the map. Place a market on the map and activate the side drawer.
    */
-  const onClick = useCallback((event: MapLayerMouseEvent): void => {
-    const feature = event?.features?.[0]
-    if (feature == null) {
-      setSelected(null)
-      setClickInfo(null)
-    } else {
-      setSelected(feature.geometry as Point | Polygon)
-      setClickInfo(transformTileProps(feature.properties as TileProps))
+  const onClick = useCallback((e: MapLibreEvent) => {
+    setMapInstance(e.target)
+    if (initialCenter != null && initialZoom != null) {
+      e.target.jumpTo({ center: initialCenter, zoom: initialZoom })
+    } else if (initialViewState != null) {
+      e.target.fitBounds(initialViewState.bounds, initialViewState.fitBoundsOptions)
     }
-  }, [mapInstance])
+    setMapLoaded(true)
+  }, [initialCenter, initialZoom, initialViewState])
 
   /**
    * Handle click event on the popover.  Behave as if the user clicked on a feature on the map.
@@ -111,11 +137,28 @@ export const GlobalMap: React.FC<GlobalMapProps> = ({
         mapLib={maplibregl}
         id='global-map'
         onLoad={onLoad}
+        initialViewState={{
+          latitude: (initialCenter != null) ? initialCenter[1] : undefined,
+          longitude: (initialCenter != null) ? initialCenter[0] : undefined,
+          zoom: initialZoom
+        }}
         onDragStart={() => {
           setCursor('move')
         }}
         onDragEnd={() => {
           setCursor('default')
+        }}
+        onMoveEnd={(e) => {
+          if (mapLoaded) {
+            const zoomLevel = Math.round(e.target.getZoom())
+            const center = e.target.getCenter()
+            const params = new URLSearchParams({
+              zoom: zoomLevel.toString(),
+              center: `${center.lat.toFixed(3)},${center.lng.toFixed(3)}`
+            })
+            const pathname = '/maps'
+            window.history.pushState({}, '', `${pathname}?${params.toString()}`)
+          }
         }}
         onMouseEnter={onHover}
         onMouseLeave={() => {
