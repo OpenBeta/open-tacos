d314662e2a22b24c18904f6c1e16d98478685037
d-k-lippert <dominik.klippert1@gmail.com>
2024-04-18 10:18:16 +0100

    change layout to vertical and add some breakpoints

--- src/components/maps/MapLayersSelector.tsx
+++ src/components/maps/MapLayersSelector.tsx
@@ -22,32 +22,32 @@ const MapLayersSelector: React.FC<Props> = ({ emit }) => {
     <>
       <Popover.Root>
         <Popover.Trigger asChild>
-          <button className='absolute bottom-20 left-3 bg-white rounded p-1 shadow'>
+          <button className='absolute bottom-20 left-3 bg-white rounded p-1.5 shadow'>
             <span>
               <img
-                className='w-14 h-14 rounded shadow'
+                className='w-12 h-12 md:w-16 md:h-16 rounded border-[1px] border-slate-300'
                 src={mapImgUrl}
                 alt='Currently selected maptiler layer'
               />
-              <span className={`text-[10px] absolute bottom-2 left-1/2 -translate-x-1/2 ${mapName === 'satellite' ? 'text-white' : ''}`}>{mapName}</span>
+              <span className={`break-word text-[0.65rem] absolute bottom-2 left-1/2 -translate-x-1/2 ${mapName === 'satellite' ? 'text-white' : ''}`}>{mapName}</span>
             </span>
           </button>
         </Popover.Trigger>
         <Popover.Portal>
-          <Popover.Content className='p-2 bg-white rounded shadow' side='right' sideOffset={20}>
-            <button className=' flex justify-between cursor-pointer'>
+          <Popover.Content className=' pt-1.5 bg-white rounded grid grid-cols-1' side='top' sideOffset={20}>
+            <button className='cursor-pointer col-span-1'>
               {Object.keys(MAP_STYLES).map((key) => {
                 const mapKey = key as keyof typeof MAP_STYLES
                 const { imgUrl } = MAP_STYLES[mapKey]
                 return (
-                  <div className='p-1' key={key} onClick={() => emitMap(key)}>
+                  <div className='px-1.5' key={key} onClick={() => emitMap(key)}>
                     <span className='grid grid-cols-1 justify-items-center'>
                       <img
-                        className='w-14 h-14 mb-2 rounded col-span-1 shadow'
+                        className='w-12 h-12 md:w-16 md:h-16 rounded col-span-1 shadow border-[1px] border-slate-300'
                         src={imgUrl}
                         alt='Currently selected maptiler layer'
                       />
-                      <span className='col-span-1 text-xs'>{key}</span>
+                      <span className='col-span-1 text-[0.65rem] '>{key}</span>
                     </span>
                   </div>
                 )
