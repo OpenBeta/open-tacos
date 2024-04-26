3a720d528cb23d22fec6d2b60f966ada12cfb408
viet nguyen <3805254+vnugent@users.noreply.github.com>
2024-04-26 17:14:08 +0200

    test shorter page cache - no-store

--- src/js/graphql/ServerClient.ts
+++ src/js/graphql/ServerClient.ts
@@ -11,10 +11,8 @@ export const { getClient } = registerApolloClient(() => {
   return new ApolloClient({
     cache: new InMemoryCache(),
     link: new HttpLink({
-      uri
-      // you can disable result caching here if you want to
-      // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
-      // fetchOptions: { cache: "no-store" },
+      uri,
+      fetchOptions: { cache: 'no-store' }
     })
   })
 })
