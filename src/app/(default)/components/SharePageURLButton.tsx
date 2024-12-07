27aea943623851e05afaf728e82472fddba0556e
Clinton Lunn <clunn@utah.gov>
2024-12-07 10:38:25 -0700

    fix: adding a default vercel url to fix the share button

--- src/app/(default)/components/SharePageURLButton.tsx
+++ src/app/(default)/components/SharePageURLButton.tsx
@@ -10,7 +10,7 @@ import { ControlledTooltip } from '@/components/ui/Tooltip'
  */
 export const SharePageURLButton: React.FC<{ path: string, name: string }> = ({ path, name }) => {
   const slug = getFriendlySlug(name)
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL != null ? process.env.NEXT_PUBLIC_BASE_URL : 'http://localhost:3000'
+  const baseUrl = process.env.VERCEL_URL != null ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000'
   const optionalSlug = slug !== '' ? `/${slug}` : ''
   const url = `${baseUrl}${path}${optionalSlug}`
 
