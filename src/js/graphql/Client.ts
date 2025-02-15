4fea3ae63ed41f34f42205bcd7022bacdc89156b
Viet Nguyen <3805254+vnugent@users.noreply.github.com>
2025-02-15 14:08:39 +0700

    fix: prevent Next cache from interferring with Apollo client

--- src/js/graphql/Client.ts
+++ src/js/graphql/Client.ts
@@ -3,7 +3,12 @@ import { onError } from '@apollo/client/link/error'
 
 const uri: string = process.env.NEXT_PUBLIC_API_SERVER ?? ''
 const httpLinkPro = new HttpLink({
-  uri
+  uri,
+  // Prevent Next.js fetch caching
+  fetchOptions: {
+    cache: 'no-store',
+    next: { revalidate: 0 }
+  }
 })
 const errorLink = onError(({ graphQLErrors, networkError, ...rest }) => {
   console.error('#################### GQL Error  ####################')
