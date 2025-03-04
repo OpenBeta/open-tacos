a0d079cd8763dd737609f2f12caa0f4017deab23
Viet Nguyen <3805254+vnugent@users.noreply.github.com>
2025-03-04 09:23:23 -0800

    separate docker build gh action

--- src/app/(maps)/maps/page.tsx
+++ src/app/(maps)/maps/page.tsx
@@ -6,7 +6,7 @@ export const dynamic = 'force-dynamic'
 
 export default async function MapPage (): Promise<any> {
   const headersList = await headers()
-  const center = parseUserLatLng(headersList.get('cf-iplatitude'), headersList.get('cf-iplongitude'))
+  const center = parseUserLatLng(headersList.get('cf-iplongitude'), headersList.get('cf-iplatitude'))
   return (
     <div className='w-full h-full'>
       <ProfileMenu />
@@ -15,7 +15,7 @@ export default async function MapPage (): Promise<any> {
   )
 }
 
-const parseUserLatLng = (lat: string | null, lng: string | null): [number, number] | undefined => {
+const parseUserLatLng = (lng: string | null, lat: string | null): [number, number] | undefined => {
   if (lng != null && lat != null) {
     return [parseFloat(lng), parseFloat(lat)]
   }
