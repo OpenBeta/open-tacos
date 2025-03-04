86de436185227c56b1a709f5ffeaf86b6c066fe5
Viet Nguyen <3805254+vnugent@users.noreply.github.com>
2025-03-04 08:26:16 -0800

    use cloudflare lat/lng

--- src/app/(maps)/components/FullScreenMap.tsx
+++ src/app/(maps)/components/FullScreenMap.tsx
@@ -5,8 +5,12 @@ import { useRouter } from 'next/navigation'
 import { MapLayerMouseEvent } from 'maplibre-gl'
 import { useUrlParams } from '@/js/hooks/useUrlParams'
 
-export const FullScreenMap: React.FC = () => {
-  const [center, setCenter] = useState<[number, number] | undefined>(undefined)
+interface FullScreenMapProps {
+  center?: [number, number]
+}
+
+export const FullScreenMap: React.FC<FullScreenMapProps> = ({ center: initialCenter }) => {
+  const [center, setCenter] = useState<[number, number] | undefined>(initialCenter)
   const [zoom, setZoom] = useState<number | undefined>(undefined)
   const [areaId, setAreaId] = useState<string | undefined>(undefined)
   const [isInitialized, setIsInitialized] = useState(false)
@@ -36,51 +40,6 @@ export const FullScreenMap: React.FC = () => {
 
     // If no camera params, get visitor location and set URL
     setZoom(DEFAULT_ZOOM)
-    getVisitorLocation()
-      .then((visitorLocation) => {
-        const newCenter: [number, number] = (visitorLocation != null)
-          ? [visitorLocation.longitude, visitorLocation.latitude]
-          : DEFAULT_CENTER
-
-        setCenter(newCenter)
-
-        // Always update URL with camera position
-        const newCamera: CameraInfo = {
-          center: {
-            lng: newCenter[0],
-            lat: newCenter[1]
-          },
-          zoom: DEFAULT_ZOOM
-        }
-
-        const url = urlParams.toUrl({
-          camera: newCamera,
-          areaId: urlAreaId
-        })
-        router.replace(url, { scroll: false })
-      })
-      .catch(() => {
-        console.log('Unable to determine user\'s location')
-        setCenter(DEFAULT_CENTER)
-
-        // Set URL with default camera position on error
-        const defaultCamera: CameraInfo = {
-          center: {
-            lng: DEFAULT_CENTER[0],
-            lat: DEFAULT_CENTER[1]
-          },
-          zoom: DEFAULT_ZOOM
-        }
-
-        const url = urlParams.toUrl({
-          camera: defaultCamera,
-          areaId: urlAreaId
-        })
-        router.replace(url, { scroll: false })
-      })
-      .finally(() => {
-        setIsInitialized(true)
-      })
   }, [urlParams, isInitialized, router])
 
   const handleCameraMovement = useCallback(
@@ -111,15 +70,4 @@ export const FullScreenMap: React.FC = () => {
       handleOnClick={handleMapClick}
     />
   )
-}
-
-const getVisitorLocation = async (): Promise<{ longitude: number, latitude: number } | undefined> => {
-  return undefined
-  // try {
-  //   const res = await fetch('/api/geo')
-  //   return await res.json()
-  // } catch (err) {
-  //   console.log('ERROR', err)
-  //   return undefined
-  // }
 }
