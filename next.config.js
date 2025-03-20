4fb59fda85f8b19f48be18ea9e93c1c528286288
Viet Nguyen <3805254+vnugent@users.noreply.github.com>
2025-03-19 22:51:45 -0700

    chore: use redis cache for prod

--- next.config.js
+++ next.config.js
@@ -94,5 +94,7 @@ module.exports = {
   },
   experimental: {
     optimizePackageImports: ['@phosphor-icons/react']
-  }
+  },
+  cacheHandler: process.env.NODE_ENV === 'production' ? require.resolve('./cache-handler.mjs') : undefined,
+  cacheMaxMemorySize: 0 // disable default in-memory caching
 }
