22a1dd74b22af5bb490d3cff495963da2f7b7fa7
Clinton Lunn <clunn@utah.gov>
2025-12-09 20:51:39 -0700

    fix(apollo): use registerApolloClient to prevent memory leak (#1422)

--- src/js/graphql/ServerClient.ts
+++ src/js/graphql/ServerClient.ts
@@ -1,5 +1,9 @@
-import { ApolloClient, HttpLink, InMemoryCache, from } from '@apollo/client'
-import { registerApolloClient } from '@apollo/experimental-nextjs-app-support'
+import { ApolloLink, HttpLink } from '@apollo/client'
+import {
+  registerApolloClient,
+  ApolloClient,
+  InMemoryCache
+} from '@apollo/client-integration-nextjs'
 import { dynamicTagsLink } from './dynamicTagsLink'
 
 const uri: string = process.env.OPENBETA_API_SERVER ?? ''
@@ -12,14 +16,31 @@ console.log('###################################################################
 console.log(' API Server', uri)
 console.log('#######################################################################')
 
-const httpLink = new HttpLink({ uri })
+const httpLink = new HttpLink({
+  uri,
+  fetchOptions: {
+    signal: AbortSignal.timeout(30000) // 30 second timeout
+  }
+})
 
 /**
- * Apollo client to be used in React Server Components.
+ * Apollo client for React Server Components.
+ * Uses registerApolloClient to share one client instance per request.
  */
-export const { getClient } = registerApolloClient(() => {
+export const { getClient, query, PreloadQuery } = registerApolloClient(() => {
   return new ApolloClient({
     cache: new InMemoryCache(),
-    link: from([dynamicTagsLink, httpLink])
+    link: ApolloLink.from([dynamicTagsLink, httpLink])
   })
 })
+
+/**
+ * Apollo client for non-RSC contexts (API routes, auth callbacks).
+ * Creates a fresh client per call.
+ */
+export function getGlobalClient (): ApolloClient<any> {
+  return new ApolloClient({
+    cache: new InMemoryCache(),
+    link: ApolloLink.from([dynamicTagsLink, httpLink])
+  })
+}
