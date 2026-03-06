80353eea97f3bef4193158b21d1f17d463efd80c
Mike Schennum <mikeschenphoto@gmail.com>
2026-03-06 11:23:18 -0700

    change recent contributions to a link

--- src/app/(default)/page.tsx
+++ src/app/(default)/page.tsx
@@ -1,13 +1,12 @@
-import { Suspense } from 'react'
 import { LandingHero } from './components/LandingHero'
 // import { getChangeHistoryServerSide } from '@/js/graphql/contribAPI'
-import { LatestContributions, LatestContributionsSkeleton } from './components/LatestContributions'
+import { LatestContributions } from './components/LatestContributions'
 import { FinancialContributors } from './components/FinancialContributors'
 import { RecentTags } from './components/RecentTags'
 import { USAToC } from './components/USAToC'
 import { InternationalToC } from './components/InternationalToC'
 import { Volunteers } from './components/Volunteers'
-import { RecentContributionsMap } from './components/recent/RecentContributionsMap'
+// import { RecentContributionsMap } from './components/recent/RecentContributionsMap'
 // import { ChangesetType } from '@/js/types'
 
 export const revalidate = 3600 // 1 hour
@@ -24,7 +23,7 @@ export default async function Home (): Promise<any> {
   //   console.error('Failed to fetch change history during build:', error)
   //   // Continue with empty history if API is down
   // }
-  const history: never[] = []
+  // const history: never[] = []
   return (
     <>
       <div className='default-page-margins flex flex-col justify-center w-fit'>
@@ -32,17 +31,11 @@ export default async function Home (): Promise<any> {
       </div>
       <div className='default-page-margins flex flex-col gap-y-12 mb-16'>
         <RecentTags />
-        <div className='lg:grid lg:grid-cols-3 gap-x-2'>
-          <div className='lg:overflow-y-auto lg:h-[450px] w-full border-2 rounded-box'>
-            <Suspense fallback={<LatestContributionsSkeleton />}>
-              <LatestContributions />
-            </Suspense>
-          </div>
-
-          <div className='lg:col-span-2 h-[450px]'>
+        <LatestContributions />
+        {/* Temporarily disabled to test if this is causing build failures */}
+        {/* <div className='lg:col-span-2 h-[450px]'>
             <RecentContributionsMap history={history} />
-          </div>
-        </div>
+          </div> */}
         <InternationalToC />
         <USAToC />
         <FinancialContributors />
