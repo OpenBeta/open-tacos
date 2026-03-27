ba650f5222c6316a7f90f0d397cc8bc6db548c98
Jonah Ssegawa <jonahssegawa44532@gmail.com>
2026-03-27 18:42:36 +0300

    fix(AppAlert): prevent layout shift on initial render for new users

--- src/components/broadcast/AppAlert.tsx
+++ src/components/broadcast/AppAlert.tsx
@@ -16,7 +16,7 @@ export const AppAlert: React.FC<AppAlertProps> = ({
   message,
   cookieStorageKey
 }) => {
-  const [showAlert, setShowAlert] = useState(false)
+  const [showAlert, setShowAlert] = useState(true)
 
   useEffect(() => {
     const suppressed = Cookies.get(cookieStorageKey)
